33d49a5b00f221370b5a43ad423782f6233c2f78
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-04-20 02:55:30 -0400

    Efield: functionalize

--- matlab/setup/Efield_BCs_3d.m
+++ matlab/setup/Efield_BCs_3d.m
@@ -1,10 +1,10 @@
-function E = Efield_BCs_3d(p, xg)
+function Efield_BCs_3d(p, xg)
 
 narginchk(2, 2)
 validateattributes(p, {'struct'}, {'scalar'}, mfilename, 'sim parameters', 1)
 validateattributes(xg, {'struct'}, {'scalar'})
 
-dir_out = absolute_path([p.simdir, '/inputs/Efield_inputs']);
+dir_out = absolute_path(p.E0_dir);
 makedir(dir_out);
 
 lx1 = xg.lx(1);
@@ -26,6 +26,8 @@ mlatmin = 90-thetamax*180/pi;
 mlatmax = 90-thetamin*180/pi;
 mlonmin = min(xg.phi(:))*180/pi;
 mlonmax = max(xg.phi(:))*180/pi;
+
+% add a 1% buff
 latbuf = 1/100 * (mlatmax-mlatmin);
 lonbuf = 1/100 * (mlonmax-mlonmin);
 E.mlat = linspace(mlatmin-latbuf, mlatmax+latbuf, E.llat);
@@ -47,13 +49,19 @@ Nt = length(time);
 UTsec = p.UTsec0 + time;     %time given in file is the seconds from beginning of hour
 UThrs = UTsec / 3600;
 E.expdate = cat(2, repmat(p.ymd(:)',[Nt, 1]), UThrs', zeros(Nt, 1), zeros(Nt, 1));
-% t=datenum(E.expdate);
+% t = datenum(E.expdate);
 %% CREATE DATA FOR BACKGROUND ELECTRIC FIELDS
-E.Exit = zeros(E.llon, E.llat, Nt);
-E.Eyit = zeros(E.llon, E.llat, Nt);
-
+if isfield(p, 'Exit')
+  E.Exit = p.Exit * ones(E.llon, E.llat, Nt);
+else
+  E.Exit = zeros(E.llon, E.llat, Nt);
+end
+if isfield(p, 'Eyit')
+  E.Eyit = p.Eyit * ones(E.llon, E.llat, Nt);
+else
+  E.Eyit = zeros(E.llon, E.llat, Nt);
+end
 %% CREATE DATA FOR BOUNDARY CONDITIONS FOR POTENTIAL SOLUTION
-p.flagdirich = 1;   %if 0 data is interpreted as FAC, else we interpret it as potential
 E.Vminx1it = zeros(E.llon,E.llat, Nt);
 E.Vmaxx1it = zeros(E.llon,E.llat, Nt);
 %these are just slices
@@ -63,30 +71,20 @@ E.Vminx3ist = zeros(E.llon, Nt);
 E.Vmaxx3ist = zeros(E.llon, Nt);
 
 %% synthesize feature
-if isfield(E, 'Etarg')    %MZ - shouldn't this be p.Etarg???
+if isfield(p, 'Etarg')
   E = Efield_target(p, xg, lx2, lx3, Nt, E);
-elseif isfield(E, 'Jtarg')
+elseif isfield(p, 'Jtarg')
   E = Jcurrent_target(p, Nt, E);
 else
   error('Efield_BCs_3d: unknown target feature')
 end
-
-%% check for NaNs
-nan_check(E)
 
 %% SAVE THESE DATA TO APPROPRIATE FILES
 % LEAVE THE SPATIAL AND TEMPORAL INTERPOLATION TO THE
 % FORTRAN CODE IN CASE DIFFERENT GRIDS NEED TO BE TRIED.
 % THE EFIELD DATA DO NOT TYPICALLY NEED TO BE SMOOTHED.
 
-switch p.file_format
-  case {'raw', 'dat'}, writeraw(dir_out, E, p)
-  case {'h5', 'hdf5'}, writehdf5(dir_out, E, p)
-  case {'nc'}, error('for NetCDF4 see gemini repo python code')
-  otherwise, error(['unknown data format ', p.file_format])
-end
-
-if ~nargout, clear('E'), end
+write_Efield(p, E, dir_out)
 
 end % function
 
@@ -115,109 +113,6 @@ end
 % x2ctr = 1/2*(xg.x2(lx2) + xg.x2(1));
 for i = 1:Nt
   E.Vmaxx1it(:,:,i) = S .* erf((E.MLON - p.mlonmean) / p.mlonsig) .* erf((E.MLAT - p.mlatmean) / p.mlatsig);
-end
-
-end % function
-
-
-function nan_check(E)
-
-%% this is also done in Fortran, but just to help ensure results.
-assert(all(isfinite(E.Exit(:))), 'NaN in Exit')
-assert(all(isfinite(E.Eyit(:))), 'NaN in Eyit')
-assert(all(isfinite(E.Vminx1it(:))), 'NaN in Vminx1it')
-assert(all(isfinite(E.Vmaxx1it(:))), 'NaN in Vmaxx1it')
-assert(all(isfinite(E.Vminx2ist(:))), 'NaN in Vminx2ist')
-assert(all(isfinite(E.Vmaxx2ist(:))), 'NaN in Vmaxx2ist')
-assert(all(isfinite(E.Vminx3ist(:))), 'NaN in Vminx3ist')
-assert(all(isfinite(E.Vmaxx3ist(:))), 'NaN in Vmaxx3ist')
-end % function
-
-
-function writehdf5(dir_out, E, p)
-narginchk(3,3)
-
-fn = [dir_out, '/simsize.h5'];
-if is_file(fn), delete(fn), end
-h5save(fn, '/llon', int32(E.llon))
-h5save(fn, '/llat', int32(E.llat))
-
-fn = [dir_out, '/simgrid.h5'];
-if is_file(fn), delete(fn), end
-
-freal = 'float32';
-
-h5save(fn, '/mlon', E.mlon, [], freal)
-h5save(fn, '/mlat', E.mlat, [], freal)
-
-Nt = size(E.expdate, 1);
-for i = 1:Nt
-  UTsec = E.expdate(i, 4)*3600 + E.expdate(i,5)*60 + E.expdate(i,6);
-  ymd = E.expdate(i, 1:3);
-
-  fn = [dir_out, filesep, datelab(ymd,UTsec), '.h5'];
-  disp(['write: ', fn])
-
-  %FOR EACH FRAME WRITE A BC TYPE AND THEN OUTPUT BACKGROUND AND BCs
-  h5save(fn, '/flagdirich', int32(p.flagdirich))
-  h5save(fn, '/Exit', E.Exit(:,:,i), [], freal)
-  h5save(fn, '/Eyit', E.Eyit(:,:,i), [], freal)
-  h5save(fn, '/Vminx1it', E.Vminx1it(:,:,i), [], freal)
-  h5save(fn, '/Vmaxx1it', E.Vmaxx1it(:,:,i), [], freal)
-  h5save(fn, '/Vminx2ist', E.Vminx2ist(:,i), [], freal)
-  h5save(fn, '/Vmaxx2ist', E.Vmaxx2ist(:,i), [], freal)
-  h5save(fn, '/Vminx3ist', E.Vminx3ist(:,i), [], freal)
-  h5save(fn, '/Vmaxx3ist', E.Vmaxx3ist(:,i), [], freal)
-end
-end % function
-
-
-function writeraw(dir_out, E, p)
-narginchk(3,3)
-
-freal = ['float', int2str(p.realbits)];
-
-fid = fopen([dir_out, '/simsize.dat'], 'w');
-fwrite(fid, E.llon, 'integer*4');
-fwrite(fid, E.llat, 'integer*4');
-fclose(fid);
-
-fid = fopen([dir_out, '/simgrid.dat'], 'w');
-fwrite(fid, E.mlon, freal);
-fwrite(fid, E.mlat, freal);
-fclose(fid);
-
-Nt = size(E.expdate, 1);
-for i = 1:Nt
-  UTsec = E.expdate(i,4)*3600 + E.expdate(i,5)*60 + E.expdate(i,6);
-  ymd = E.expdate(i,1:3);
-  filename = [dir_out, filesep, datelab(ymd,UTsec), '.dat'];
-  disp(['write: ',filename])
-  fid = fopen(filename, 'w');
-
-  %These all check out fine...
-%   size(E.Exit)
-%   size(E.Eyit)
-%   size(E.Vminx1it)
-%   size(E.Vmaxx1it)
-%   size(E.Vminx2ist)
-%   size(E.Vmaxx2ist)
-%   size(E.Vminx3ist)
-%   size(E.Vmaxx3ist)
-
-  %FOR EACH FRAME WRITE A BC TYPE AND THEN OUTPUT BACKGROUND AND BCs
-%  fwrite(fid, p.flagdirich, 'int32');
-  fwrite(fid, p.flagdirich, freal);   %FIXME - fortran code still wants this to be real...
-  fwrite(fid, E.Exit(:,:,i), freal);
-  fwrite(fid, E.Eyit(:,:,i), freal);
-  fwrite(fid, E.Vminx1it(:,:,i), freal);
-  fwrite(fid, E.Vmaxx1it(:,:,i), freal);
-  fwrite(fid, E.Vminx2ist(:,i), freal);
-  fwrite(fid, E.Vmaxx2ist(:,i), freal);
-  fwrite(fid, E.Vminx3ist(:,i), freal);
-  fwrite(fid, E.Vmaxx3ist(:,i), freal);
-
-  fclose(fid);
 end
 
 end % function
---
+++ matlab/setup/write_Efield.m
@@ -0,0 +1,107 @@
+function write_Efield(cfg, E, dir_out)
+
+narginchk(3,3)
+
+nan_check(E)
+
+switch cfg.file_format
+  case {'raw', 'dat'}, write_raw(dir_out, E, cfg)
+  case {'h5', 'hdf5'}, write_hdf5(dir_out, E, cfg)
+  case {'nc'}, error('for NetCDF4 see gemini repo python code')
+  otherwise, error(['unknown data format ', cfg.file_format])
+end
+
+end
+
+
+function nan_check(E)
+
+%% this is also done in Fortran, but just to help ensure results.
+assert(all(isfinite(E.Exit(:))), 'NaN in Exit')
+assert(all(isfinite(E.Eyit(:))), 'NaN in Eyit')
+assert(all(isfinite(E.Vminx1it(:))), 'NaN in Vminx1it')
+assert(all(isfinite(E.Vmaxx1it(:))), 'NaN in Vmaxx1it')
+assert(all(isfinite(E.Vminx2ist(:))), 'NaN in Vminx2ist')
+assert(all(isfinite(E.Vmaxx2ist(:))), 'NaN in Vmaxx2ist')
+assert(all(isfinite(E.Vminx3ist(:))), 'NaN in Vminx3ist')
+assert(all(isfinite(E.Vmaxx3ist(:))), 'NaN in Vmaxx3ist')
+end % function
+
+
+function write_hdf5(dir_out, E, p)
+narginchk(3,3)
+
+fn = [dir_out, '/simsize.h5'];
+if is_file(fn), delete(fn), end
+h5save(fn, '/llon', int32(E.llon))
+h5save(fn, '/llat', int32(E.llat))
+
+fn = [dir_out, '/simgrid.h5'];
+if is_file(fn), delete(fn), end
+
+freal = 'float32';
+
+h5save(fn, '/mlon', E.mlon, [], freal)
+h5save(fn, '/mlat', E.mlat, [], freal)
+
+disp(['write to ', dir_out])
+Nt = size(E.expdate, 1);
+for i = 1:Nt
+  UTsec = E.expdate(i, 4)*3600 + E.expdate(i,5)*60 + E.expdate(i,6);
+  ymd = E.expdate(i, 1:3);
+
+  fn = [dir_out, filesep, datelab(ymd,UTsec), '.h5'];
+
+  %FOR EACH FRAME WRITE A BC TYPE AND THEN OUTPUT BACKGROUND AND BCs
+  h5save(fn, '/flagdirich', int32(p.Eflagdirich))
+  h5save(fn, '/Exit', E.Exit(:,:,i), [], freal)
+  h5save(fn, '/Eyit', E.Eyit(:,:,i), [], freal)
+  h5save(fn, '/Vminx1it', E.Vminx1it(:,:,i), [], freal)
+  h5save(fn, '/Vmaxx1it', E.Vmaxx1it(:,:,i), [], freal)
+  h5save(fn, '/Vminx2ist', E.Vminx2ist(:,i), [], freal)
+  h5save(fn, '/Vmaxx2ist', E.Vmaxx2ist(:,i), [], freal)
+  h5save(fn, '/Vminx3ist', E.Vminx3ist(:,i), [], freal)
+  h5save(fn, '/Vmaxx3ist', E.Vmaxx3ist(:,i), [], freal)
+end
+end % function
+
+
+function write_raw(dir_out, E, p)
+narginchk(3,3)
+
+freal = ['float', int2str(p.realbits)];
+
+fid = fopen([dir_out, '/simsize.dat'], 'w');
+fwrite(fid, E.llon, 'integer*4');
+fwrite(fid, E.llat, 'integer*4');
+fclose(fid);
+
+fid = fopen([dir_out, '/simgrid.dat'], 'w');
+fwrite(fid, E.mlon, freal);
+fwrite(fid, E.mlat, freal);
+fclose(fid);
+
+Nt = size(E.expdate, 1);
+for i = 1:Nt
+  UTsec = E.expdate(i,4)*3600 + E.expdate(i,5)*60 + E.expdate(i,6);
+  ymd = E.expdate(i,1:3);
+  filename = [dir_out, filesep, datelab(ymd,UTsec), '.dat'];
+  disp(['write: ',filename])
+  fid = fopen(filename, 'w');
+
+  %FOR EACH FRAME WRITE A BC TYPE AND THEN OUTPUT BACKGROUND AND BCs
+%  fwrite(fid, p.flagdirich, 'int32');
+  fwrite(fid, p.flagdirich, freal);   %FIXME - fortran code still wants this to be real...
+  fwrite(fid, E.Exit(:,:,i), freal);
+  fwrite(fid, E.Eyit(:,:,i), freal);
+  fwrite(fid, E.Vminx1it(:,:,i), freal);
+  fwrite(fid, E.Vmaxx1it(:,:,i), freal);
+  fwrite(fid, E.Vminx2ist(:,i), freal);
+  fwrite(fid, E.Vmaxx2ist(:,i), freal);
+  fwrite(fid, E.Vminx3ist(:,i), freal);
+  fwrite(fid, E.Vmaxx3ist(:,i), freal);
+
+  fclose(fid);
+end
+
+end % function
