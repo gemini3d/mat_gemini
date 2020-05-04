9653f2ce3fba00351663f979892ddab45abe5f94
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-05-04 15:19:36 -0400

    add model_setup self-test

--- tests/test_matlab.m
+++ tests/test_matlab.m
@@ -5,7 +5,7 @@ addpath([cwd,'/../matlab'])
 
 %% config.nml load test
 
-p = read_config([cwd, '/config.nml']);
+p = read_config([cwd, '/test2d_fang/config.nml']);
 
 assert(p.xdist == 200000)
 
@@ -15,6 +15,11 @@ assert(p.xdist == 200000)
 assert(all(ymd == [2012, 3, 27]))
 assert(utsec == 1500.5)
 
+%% test2d_eq
+model_setup('test2d_eq')
+
+%% test2d_fang
+model_setup('test2d_fang')
 
 % done
 disp('OK: gemini-matlab tests')
