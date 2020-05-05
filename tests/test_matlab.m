5dce173ad6bb3fa41aec20ce09eee8d042134427
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-05-05 05:46:26 -0400

    test ci setup

--- tests/test_matlab.m
+++ tests/test_matlab.m
@@ -1,7 +1,7 @@
 % test basic functions
 
 cwd = fileparts(mfilename('fullpath'));
-addpath([cwd,'/../matlab'])
+run([cwd, '/../setup.m'])
 
 %% config.nml load test
 
@@ -16,7 +16,11 @@ assert(all(ymd == [2012, 3, 27]))
 assert(utsec == 1500.5)
 
 %% test2d_eq
-model_setup('test2d_eq')
+if exist('h5create', 'file')
+  model_setup('test2d_eq')
+else
+  disp('SKIP: missing HDF5')
+end
 
 % done
 disp('OK: gemini-matlab tests')
