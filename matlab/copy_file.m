89c0221af919650118fb5201d2c71253f66ea6e2
Michael Hirsch <scivision@users.noreply.github.com>
2020-05-06 17:58:28 -0400

    warn not error if try to copy file on itself

--- matlab/copy_file.m
+++ matlab/copy_file.m
@@ -1,5 +1,12 @@
 function copy_file(in, out)
 %% copy_file(path) overloads copyfile with tilde expansion
+narginchk(2,2)
 
+fin = absolute_path(in);
+fout = absolute_path(out);
+if strcmp(fileparts(fin), fout)
+  fprintf(2, 'skipping copy of file onto itself: %s\n', fin)
+  return
+end
 copyfile(absolute_path(in), absolute_path(out))
 end
