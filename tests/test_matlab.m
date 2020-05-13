e8682cabc3acefd84fb05f108d15a0c07bb7825c
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-05-13 13:54:22 -0400

    increase test coverage

--- tests/test_matlab.m
+++ tests/test_matlab.m
@@ -1,11 +1,15 @@
 % test basic functions
+
+cwd = fileparts(mfilename('fullpath'));
+run([cwd, '/../setup.m'])
+%% setup
 if exist('checkcode', 'file')
-  checkcode_recursive(fullfile(cwd,'..'))
+  checkcode_recursive([cwd, '/../'])
+else
+  fprintf(2, 'SKIP: lint check\n')
 end
 
 tic
-cwd = fileparts(mfilename('fullpath'));
-run([cwd, '/../setup.m'])
 %% config.nml load test
 
 p = read_config([cwd, '/test2dew_fang/config.nml']);
@@ -22,7 +26,7 @@ assert(utsec == 1500.5)
 if exist('h5create', 'file')
   model_setup('test2dew_eq')
 else
-  disp('SKIP: missing HDF5')
+  fprintf(2, 'SKIP: missing HDF5')
 end
 
 %% test2d_eq_nc4
@@ -33,6 +37,14 @@ if exist('nccreate', 'file')
   model_setup('test2dew_eq/config_nc4.nml')
 else
   disp('SKIP: missing NetCDF4')
+end
+%% test2d_fang
+try
+  model_setup('test2dew_fang')
+catch e
+  if ~strcmp(e.identifier, 'readgrid:file_not_found')
+    rethrow(e)
+  end
 end
 
 % done
