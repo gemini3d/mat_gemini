c95d29688a7041ee2f1a069ca21da29896120f57
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-05-18 15:31:13 -0400

    more robust windows cmake

--- matlab/cmake.m
+++ matlab/cmake.m
@@ -4,23 +4,33 @@ function cmake(srcdir, builddir)
 
 narginchk(2,2)
 
-ret = system(['cmake --version']);
+ret = system('cmake --version');
 if ret ~= 0
   error('cmake:environment_error', 'CMake not found')
 end
 
-ret = system(['cmake -S', srcdir, ' -B', builddir]);
+tail = '';
+if ispc && isempty(getenv('CMAKE_GENERATOR'))
+  ret = system('ninja --version');
+  if ret==0
+    tail = ' -G Ninja';
+  else
+    tail = ' -G MinGW Makefiles';
+  end
+end
+
+ret = system(['cmake -S', srcdir, ' -B', builddir, tail]);
 if ret ~= 0
   delete(fullfile(builddir, 'CMakeCache.txt'))
-  ret = system(['cmake -S', srcdir, ' -B', builddir]);
+  ret = system(['cmake -S', srcdir, ' -B', builddir, tail]);
 end
 if ret~=0
-  error('cmake:runtime_error', 'error configuring MSIS')
+  error('cmake:runtime_error', 'error configuring with CMake')
 end
 
 ret = system(['cmake --build ',builddir,' --parallel']);
 if ret ~= 0
-  error('cmake:runtime_error', 'error building MSIS')
+  error('cmake:runtime_error', 'error building with CMake')
 end
 
 end % function
