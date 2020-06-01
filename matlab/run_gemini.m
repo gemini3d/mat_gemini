dbc52faf9cf0deafe87e636fb58d615511683442
Michael Hirsch <scivision@users.noreply.github.com>
2020-06-01 15:22:17 -0400

    add run_gemini sanity check

--- matlab/run_gemini.m
+++ matlab/run_gemini.m
@@ -1,12 +1,18 @@
-function job(cfgfile, outdir)
+function run_gemini(cfgfile, outdir)
 % consider the full-featured gemini/job.py
 narginchk(2,2)
 
 cwd = fileparts(mfilename('fullpath'));
-addpath(fullfile(cwd, 'matlab'))
+addpath(cwd)
 
-np = maxNumCompThreads;
-
+if isoctave
+  % NOTE: due to Octave on Windows overriding system MinGW,
+  % you may need to compile Gemini.bin with -static option.
+  % this was not fixed by addpath(getenv('MINGWROOT'))
+  np = idivide(nproc, 2);  % assume hyperthreading
+else
+  np = maxNumCompThreads;
+end
 %% get gemini.bin executable
 gemini_root = getenv('GEMINI_ROOT');
 if isempty(gemini_root)
@@ -24,6 +30,10 @@ assert(ret == 0, 'mpiexec not found')
 %% assemble command
 cfgfile = get_configfile(cfgfile);
 cmd = sprintf('mpiexec -n %d %s %s %s', np, gemexe, cfgfile, outdir);
+disp(cmd)
+%% sanity check gemini.bin executable
+ret = system(gemexe);
+assert(ret==77, ['problem with ', gemexe])
 %% dry run
 ret = system([cmd, ' -dryrun']);
 assert(ret==77, 'Gemini dryrun failed')
