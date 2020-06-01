c384ae086a9208712b5b58d043ecab5ce34d4316
Michael Hirsch <scivision@users.noreply.github.com>
2020-06-01 15:04:43 -0400

    add run_gemini

--- matlab/run_gemini.m
+++ matlab/run_gemini.m
@@ -1,5 +1,5 @@
 function job(cfgfile, outdir)
-% note, one should instead use gemini/job.py
+% consider the full-featured gemini/job.py
 narginchk(2,2)
 
 cwd = fileparts(mfilename('fullpath'));
@@ -7,21 +7,27 @@ addpath(fullfile(cwd, 'matlab'))
 
 np = maxNumCompThreads;
 
-gemini_root = absolute_path(fullfile(cwd, '../gemini'));
+%% get gemini.bin executable
+gemini_root = getenv('GEMINI_ROOT');
+if isempty(gemini_root)
+  gemini_root = absolute_path(fullfile(cwd, '../gemini'));
+end
 assert(isfolder(gemini_root), 'Gemini3D directory not found')
 gemexe = fullfile(gemini_root, 'build/gemini.bin');
 if ispc
   gemexe = [gemexe, '.exe'];
 end
 assert(isfile(gemexe), 'Gemini.bin executable not found')
-
+%% ensure mpiexec is available
 [ret, ~] = system('mpiexec -help');
 assert(ret == 0, 'mpiexec not found')
-
-cfgfile = get_configfile(cfgfile);
 %% assemble command
-cmd = sprintf('mpiexec -n %d %s %s %s -dryrun', np, gemexe, cfgfile, outdir);
-ret = system(cmd);
+cfgfile = get_configfile(cfgfile);
+cmd = sprintf('mpiexec -n %d %s %s %s', np, gemexe, cfgfile, outdir);
+%% dry run
+ret = system([cmd, ' -dryrun']);
 assert(ret==77, 'Gemini dryrun failed')
-
-end
+%% run simulation
+ret = system(cmd);
+assert(ret==0, 'Gemini run failed')
+end % function
