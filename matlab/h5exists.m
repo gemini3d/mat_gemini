45de0ac26c9120ca592ff2b3cf3584fb6e49c709
Michael Hirsch <scivision@users.noreply.github.com>
2020-06-05 16:18:17 -0400

    add h5exists.m

---
+++ matlab/h5exists.m
@@ -0,0 +1,28 @@
+function exists = h5exists(filename, variable)
+% check if variable exists in HDF5 filename
+%
+% filename: HDF5 filename
+% variable: name of variable inside HDF5 file
+%
+% exists: boolean
+
+narginchk(2,2)
+validateattributes(variable, {'char'}, {'vector'}, 2)
+
+exists = false;
+
+if ~is_file(filename)
+  warning([filename, ' does not exist'])
+  return
+end
+
+try
+  finf = h5info(filename, variable);
+  exists = true;
+catch excp % variable not exist
+  if ~any(strcmp(excp.identifier, {'MATLAB:imagesci:h5info:fileOpenErr', 'MATLAB:imagesci:h5info:unableToFind'}))
+    rethrow(excp)
+  end
+end % try
+
+end % function
