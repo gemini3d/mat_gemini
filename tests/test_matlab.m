d3d4177b8a0f0678b98fc10779e6d108ecf1d00b
Michael Hirsch <scivision@users.noreply.github.com>
2020-06-08 16:13:48 -0400

    add get_max_mpi.m

--- tests/test_matlab.m
+++ tests/test_matlab.m
@@ -14,15 +14,25 @@ else
 end
 
 tic
-%% config.nml load test
+%% config.nml unit test
 p = read_config(fullfile(cwd, 'test2dew_fang', 'config.nml'));
 assert(p.xdist == 200000)
 q = read_config(fullfile(cwd, 'test2dew_fang'));
 assert(isequaln(p, q), 'file or folder should give same result')
-%% dateinc test
+%% dateinc unit test
 [ymd, utsec] = dateinc(0.5, [2012,3,27], 1500);
 assert(all(ymd == [2012, 3, 27]))
 assert(utsec == 1500.5)
+%% max mpi unit test
+assert(max_mpi([48,1,40], 5) == 5, 'max_mpi fail 5 cpu')
+assert(max_mpi([48,40,1], 5) == 5, 'max_mpi fail 5 cpu')
+assert(max_mpi([48,1,40], 6) == 5, 'max_mpi fail 6 cpu')
+assert(max_mpi([48,40,1], 6) == 5, 'max_mpi fail 6 cpu')
+assert(max_mpi([48,1,40], 8) == 8, 'max_mpi fail 8 cpu')
+assert(max_mpi([48,40,1], 8) == 8, 'max_mpi fail 8 cpu')
+assert(max_mpi([48,1,40], 28) == 20, 'max_mpi fail 28 cpu')
+assert(max_mpi([48,1,40], 28) == 20, 'max_mpi fail 28 cpu')
+assert(max_mpi([48,1,36], 28) == 18, 'max_mpi fail 28 cpu')
 %% test2dew_eq_hdf5
 runner('test2dew_eq_h5')
 %% test2dew_eq_nc4
