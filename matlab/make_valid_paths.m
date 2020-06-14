40219a35be7bef1b9f8bccdb0e0603a01eb7b52d
Michael Hirsch <scivision@users.noreply.github.com>
2020-06-14 13:39:20 -0400

    make_valid_path optional top input

--- matlab/make_valid_paths.m
+++ matlab/make_valid_paths.m
@@ -1,16 +1,20 @@
-function cfg = make_valid_paths(cfg)
+function cfg = make_valid_paths(cfg, top)
 %% resolve all paths in the config struct
-narginchk(1,1)
+narginchk(1,2)
 
-cfg.input_dir = fullfile(cfg.outdir, 'inputs');
+if nargin < 2
+  top = cfg.outdir;
+end
+
+cfg.input_dir = fullfile(top, 'inputs');
 
 for n = {'indat_size', 'indat_grid', 'indat_file'}
-  cfg.(n{:}) = make_valid_filename(cfg.(n{:}), cfg.outdir);
+  cfg.(n{:}) = make_valid_filename(cfg.(n{:}), top);
 end
 
 for n = {'eqdir', 'E0_dir', 'prec_dir'}
   if isfield(cfg, n{:}) && ~isempty(cfg.(n{:}))
-    cfg.(n{:}) = make_valid_folder(cfg.(n{:}), cfg.outdir);
+    cfg.(n{:}) = make_valid_folder(cfg.(n{:}), top);
   end
 end
 
