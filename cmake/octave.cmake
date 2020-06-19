e26bd816a98cbeb5dd6b499609639afabb43ec6a
Michael Hirsch <scivision@users.noreply.github.com>
2020-06-19 11:31:27 -0400

    octave: readd netcdf unit tests

--- cmake/octave.cmake
+++ cmake/octave.cmake
@@ -5,3 +5,6 @@ endif()
 
 add_test(NAME octave:unit COMMAND ${Octave_EXECUTABLE} --eval "setup; test_unit"
   WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+
+add_test(NAME octave:netcdf COMMAND ${Octave_EXECUTABLE} --eval "setup; test_netcdf"
+  WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
