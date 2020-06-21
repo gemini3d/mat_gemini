31fff48a237bbba3a6bdc89a6e259337d836595b
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-06-20 23:42:39 -0400

    ctest: check that octave-netcdf is available

--- cmake/octave.cmake
+++ cmake/octave.cmake
@@ -6,5 +6,13 @@ endif()
 add_test(NAME octave:unit COMMAND ${Octave_EXECUTABLE} --eval "setup; test_unit"
   WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
 
+# check if octave-netcdf available
+execute_process(COMMAND ${Octave_EXECUTABLE} --eval "pkg('load', 'netcdf')"
+  RESULT_VARIABLE octave_netcdf_ok)
+
+if(octave_netcdf_ok EQUAL 0)
+
 add_test(NAME octave:netcdf COMMAND ${Octave_EXECUTABLE} --eval "setup; test_netcdf"
   WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+
+endif()
