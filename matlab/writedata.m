93562b229be245fdf4f5573ca86e34d152ff964f
Matthew Zettergren <mattzett@gmail.com>
2020-07-09 17:24:42 -0400

    updated writedata to accommodate potential inputs

--- matlab/writedata.m
+++ matlab/writedata.m
@@ -1,4 +1,5 @@
-function writedata(ymd, UTsec,ns,vsx1,Ts, fn, file_format, realbits)
+function writedata(ymd, UTsec,ns,vsx1,Ts, fn, file_format, realbits, Phitop)
+
 %% WRITE STATE VARIABLE DATA TO BE USED AS INITIAL CONDITIONS
 % FOR ANOTHER SIMULATION.  NOTE THAT WE
 % DO NOT HERE OUTPUT ANY OF THE ELECTRODYNAMIC
@@ -8,7 +9,7 @@ function writedata(ymd, UTsec,ns,vsx1,Ts, fn, file_format, realbits)
 % INPUT ARRAYS SHOULD BE TRIMMED TO THE CORRECT SIZE
 % I.E. THEY SHOULD NOT INCLUDE GHOST CELLS
 
-narginchk(7,8)
+narginchk(7,9)
 validateattributes(ymd, {'numeric'}, {'vector', 'positive', 'numel', 3}, mfilename, 'year, month, day', 1)
 validateattributes(UTsec, {'numeric'}, {'scalar', 'nonnegative'}, mfilename, 'seconds since UT midnight', 2)
 validateattributes(ns, {'numeric'}, {'ndims', 4,'nonnegative'}, mfilename, 'density', 3)
@@ -16,20 +17,27 @@ validateattributes(vsx1, {'numeric'}, {'ndims', 4}, mfilename, 'velocity', 4)
 validateattributes(Ts, {'numeric'}, {'ndims', 4,'nonnegative'}, mfilename, 'temperature', 5)
 validateattributes(fn, {'char'}, {'vector'}, mfilename, 'output filename',6)
 validateattributes(file_format, {'char'}, {'vector'}, mfilename,'hdf5 or raw',7)
-if nargin==7, realbits=64; end
+if nargin<=7
+  realbits=64;
+end
 validateattributes(realbits, {'numeric'}, {'scalar','integer'},mfilename, '32 or 64',8)
+if nargin<=8
+    Phitop=zeros(size(ns,2),size(ns,3));
+end %if
+% FIXME:  may want validate potential input
 
 switch file_format
-  case 'h5', write_hdf5(fn, ymd, UTsec, ns, vsx1, Ts)
-  case 'nc', write_nc4(fn, ymd, UTsec, ns, vsx1, Ts)
-  case 'dat', write_raw(fn, ymd, UTsec, ns, vsx1, Ts, realbits)
+  case 'h5', write_hdf5(outdir, ymd, UTsec, ns, vsx1, Ts, Phitop)
+  % Note that the raw file input does not accept potential input!!!
+  case 'dat', write_raw(outdir, ymd, UTsec, ns, vsx1, Ts, realbits)
+  case 'nc', write_nc4(outdir, ymd, UTsec, ns, vsx1, Ts, Phitop)
   otherwise, error('writedata:value_error', 'unknown file_format %s', file_format)
 end
 
 end % function
 
 
-function write_hdf5(fn, ymd, UTsec, ns, vsx1, Ts)
+function write_hdf5(fn, ymd, UTsec, ns, vsx1, Ts, Phitop)
 
 fn = with_suffix(fn, '.h5');
 
@@ -44,11 +52,12 @@ h5save(fn, '/time/UTsec', UTsec, [], freal)
 h5save(fn, '/nsall', ns, [], freal)
 h5save(fn, '/vs1all', vsx1, [], freal)
 h5save(fn, '/Tsall', Ts, [], freal)
+h5save(fn, '/Phiall', Phitop, [], freal)
 
 end % function
 
 
-function write_nc4(fn, ymd, UTsec, ns, vsx1, Ts)
+function write_nc4(fn, ymd, UTsec, ns, vsx1, Ts, Phitop)
 
 if isoctave
   pkg('load','netcdf')
@@ -63,11 +72,13 @@ ncsave(fn, 'ymd', int32(ymd), {'time', 3})
 
 freal = 'float32';
 dimspec = {'x1', size(ns, 1), 'x2', size(ns, 2), 'x3', size(ns,3), 'species', 7};
+dimspec3 = {'x1', size(ns, 1), 'x2', size(ns, 2), 'x3', size(ns,3)};
 
 ncsave(fn, 'UTsec', UTsec)
 ncsave(fn, 'ns', ns, dimspec, freal)
 ncsave(fn, 'vsx1', vsx1, dimspec, freal)
 ncsave(fn, 'Ts', Ts, dimspec, freal)
+ncsave(fn, 'Phi', Phitop, dimspec3, freal)
 
 end % function
 
