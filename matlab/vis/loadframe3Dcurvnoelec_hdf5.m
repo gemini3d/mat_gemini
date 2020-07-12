f68ab97e40b6bd2db59a865525adcdd3b30f02fa
Michael Hirsch <scivision@users.noreply.github.com>
2020-07-12 10:33:13 -0400

    finish merge

--- matlab/vis/loadframe3Dcurvnoelec_hdf5.m
+++ matlab/vis/loadframe3Dcurvnoelec_hdf5.m
@@ -7,48 +7,48 @@ dat.filename = filename;
 
 if isoctave
   D = load(filename);
-  %dat.simdate(1:3) = D.time.ymd;
-  %dat.simdate(4) = D.time.UThour;
   dat.ns = D.nsall;
   dat.vs1 = D.vs1all;
   dat.Ts = D.Tsall;
-else
+	return
+end
 
-  [varnames, grpnames] = h5variables(filename);
+[varnames, grpnames] = h5variables(filename);
 
-%  if any(strcmp('time', grpnames))
+if any(strcmp('ymd', varnames))
+  if any(strcmp('time', grpnames))
     i = '/time';
-%  else
-%    i = '';
-%  end
-
+  else
+    i = '';
+  end
   dat.simdate(1:3) = double(h5read(filename, [i, '/ymd']));
 
   try
     dat.simdate(4) = h5read(filename, [i, '/UThour']);
   catch
     dat.simdate(4) = h5read(filename, [i, '/UTsec']) / 3600;
-  if any(strcmp('nsall', varnames))
-    i = '/nsall';
-  else
-    i = '/ns';
   end
-  dat.ns = h5read(filename, i);
-
-  if any(strcmp('vs1all', varnames))
-    i = '/vs1all';
-  else
-    i = '/vsx1';
-  end
-  dat.vs1 = h5read(filename, i);
-
-  if any(strcmp('Tsall', varnames))
-    i = '/Tsall';
-  else
-    i = '/Ts';
-  end
-  dat.Ts = h5read(filename, i);
-
 end
+
+if any(strcmp('nsall', varnames))
+  i = '/nsall';
+else
+  i = '/ns';
+end
+dat.ns = h5read(filename, i);
+
+if any(strcmp('vs1all', varnames))
+  i = '/vs1all';
+else
+  i = '/vsx1';
+end
+dat.vs1 = h5read(filename, i);
+
+if any(strcmp('Tsall', varnames))
+  i = '/Tsall';
+else
+  i = '/Ts';
+end
+dat.Ts = h5read(filename, i);
 
 end % function
