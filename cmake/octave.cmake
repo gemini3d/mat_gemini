bdc29e103352b517ecf05cf90fd29367a82e381e
Michael Hirsch <scivision@users.noreply.github.com>
2020-07-12 16:09:31 -0400

    use ctest -S setup.cmake in parallel

--- cmake/octave.cmake
+++ cmake/octave.cmake
@@ -3,16 +3,23 @@ if(NOT Octave_FOUND)
   return()
 endif()
 
-add_test(NAME octave:unit COMMAND ${Octave_EXECUTABLE} --eval "setup; test_unit"
+add_test(NAME octave:unit
+  COMMAND ${Octave_EXECUTABLE} --eval "setup; test_unit"
   WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
 
 # check if octave-netcdf available
 execute_process(COMMAND ${Octave_EXECUTABLE} --eval "pkg('load', 'netcdf')"
   RESULT_VARIABLE octave_netcdf_ok)
 
-if(octave_netcdf_ok EQUAL 0)
+if(NOT octave_netcdf_ok EQUAL 0)
+  return()
+endif()
 
-add_test(NAME octave:netcdf COMMAND ${Octave_EXECUTABLE} --eval "setup; test_netcdf"
+add_test(NAME octave:netcdf
+  COMMAND ${Octave_EXECUTABLE} --eval "setup; test_netcdf"
   WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
 
-endif()
+# bug in Octave-netcdf writing that needs to be fixed
+# add_test(NAME octave:project
+#   COMMAND ${Octave_EXECUTABLE} --eval "setup; test_project"
+#   WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
