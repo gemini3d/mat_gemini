89e7b070319d79733021beb0374dd133e2c14e82
Michael Hirsch <scivision@users.noreply.github.com>
2020-07-12 10:32:42 -0400

    auto chunk size, more robust

--- matlab/fileio/h5variables.m
+++ matlab/fileio/h5variables.m
@@ -2,11 +2,17 @@ function [names, groups] = h5variables(filename)
 % get dataset names and groups in an HDF5 file
 narginchk(1,1)
 
+names = [];
+groups = [];
+
 filename = expanduser(filename);
 
 % use temporary variable to be R2017b OK
 finf = h5info(filename);
 ds = finf.Datasets;
+if isempty(ds)
+  return
+end
 
 names = {ds(:).Name};
 
