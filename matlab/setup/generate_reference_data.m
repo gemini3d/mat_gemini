a14dfe1a972dc0aca6b0b0b16dfd919aa85f0480
Michael Hirsch <scivision@users.noreply.github.com>
2020-07-13 03:00:49 -0400

    error if no files found

--- matlab/setup/generate_reference_data.m
+++ matlab/setup/generate_reference_data.m
@@ -15,6 +15,9 @@ assert(is_folder(topdir), '%s is not a folder', topdir)
 
 %% run each simulation
 names = get_testnames(topdir, only);
+if isempty(names)
+  error(['No inputs under ',topdir,' with ', only])
+end
 
 gem_params = struct('overwrite', true, 'mpiexec', [] , 'file_format', file_format);
 
