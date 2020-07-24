3bc4651406243b1ef9c41405e5c03306e0d4776c
Michael Hirsch <scivision@users.noreply.github.com>
2020-07-24 18:31:11 -0400

    matlab.cmake: correct syntax

--- cmake/matlab.cmake
+++ cmake/matlab.cmake
@@ -30,8 +30,8 @@ set_tests_properties(matlab:${test_name} PROPERTIES
 
 endfunction(matlab_test)
 
-matlab_test(lint "{'test_lint'}", 0)
+matlab_test(lint "{'test_lint'}" 0)
 
-matlab_test(unit "{'test_unit', 'test_hdf5', 'test_netcdf'}", 0)
+matlab_test(unit "{'test_unit', 'test_hdf5', 'test_netcdf'}" 0)
 
-matlab_test(project "{'test_project_hdf5', 'test_project_netcdf'}", 1)
+matlab_test(project "{'test_project_hdf5', 'test_project_netcdf'}" 1)
