0595f001009a88e24600b589cd54c07b67393e2e
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-07-27 17:31:10 -0400

    test_project* add new test names

--- tests/test_project_hdf5.m
+++ tests/test_project_hdf5.m
@@ -7,11 +7,17 @@ run(fullfile(cwd, '../setup.m'))
 
 tic
 %% test2dew_eq_h5
-runner('2d_eq', 'h5')
+runner('2dew_eq', 'h5')
 %% test2dew_fang_h5
-runner('2d_fang', 'h5')
+runner('2dew_fang', 'h5')
 %% test2dew_glow_h5
-runner('2d_glow', 'h5')
+runner('2dew_glow', 'h5')
+%% test2dns_eq_h5
+runner('2dns_eq', 'h5')
+%% test2dns_fang_h5
+runner('2dns_fang', 'h5')
+%% test2dns_glow_h5
+runner('2dns_glow', 'h5')
 %% test3d_eq_h5
 runner('3d_eq', 'h5')
 %% test3d_fang_h5
--- tests/test_project_netcdf.m
+++ tests/test_project_netcdf.m
@@ -11,11 +11,17 @@ end
 
 tic
 %% test2dew_eq_nc
-runner('2d_eq', 'nc')
+runner('2dew_eq', 'nc')
 %% test2dew_fang_nc
-runner('2d_fang', 'nc')
+runner('2dew_fang', 'nc')
 %% test2dew_glow_nc
-runner('2d_glow', 'nc')
+runner('2dew_glow', 'nc')
+%% test2dns_eq_nc
+runner('2dns_eq', 'nc')
+%% test2dns_fang_nc
+runner('2dns_fang', 'nc')
+%% test2dns_glow_nc
+runner('2dns_glow', 'nc')
 %% test3d_eq_nc
 runner('3d_eq', 'nc')
 %% test3d_fang_nc
