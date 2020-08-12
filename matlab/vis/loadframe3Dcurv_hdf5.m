c7a0b4ee6b7dfb0d1cfd19fd776fb4eec0a63cc1
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-08-12 04:16:07 -0400

    loadframe: tolerate partial variable set

--- matlab/vis/loadframe3Dcurv_hdf5.m
+++ matlab/vis/loadframe3Dcurv_hdf5.m
@@ -2,22 +2,24 @@ function dat = loadframe3Dcurv_hdf5(fn)
 
 narginchk(1,1)
 %% SIMULATION RESULTS
+vars = h5variables(fn);
+
 dat.filename = fn;
 
 dat.ns = h5read(fn, '/nsall');
 dat.vs1 = h5read(fn, '/vs1all');
 dat.Ts = h5read(fn, '/Tsall');
-dat.J1 = h5read(fn, '/J1all');
-dat.J2 = h5read(fn, '/J2all');
-dat.J3 = h5read(fn, '/J3all');
-dat.v2 = h5read(fn, '/v2avgall');
-dat.v3 = h5read(fn, '/v3avgall');
-dat.Phitop = h5read(fn, '/Phiall');
-
-dat.J1 = squeeze(dat.J1);
-dat.J2 = squeeze(dat.J2);
-dat.J3 = squeeze(dat.J3);
-dat.v2 = squeeze(dat.v2);
-dat.v3 = squeeze(dat.v3);
+if any(contains(vars, 'J1all'))
+  dat.J1 = squeeze(h5read(fn, '/J1all'));
+  dat.J2 = squeeze(h5read(fn, '/J2all'));
+  dat.J3 = squeeze(h5read(fn, '/J3all'));
+end
+if any(contains(vars, 'v2avgall'))
+  dat.v2 = squeeze(h5read(fn, '/v2avgall'));
+  dat.v3 = squeeze(h5read(fn, '/v3avgall'));
+end
+if any(contains(vars, 'Phiall'))
+  dat.Phitop = h5read(fn, '/Phiall');
+end
 
 end % function
