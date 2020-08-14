53351e051bf9d91f904c858119f30f4ca0cc2b07
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-08-14 16:53:28 -0400

    make gemini_plot easier interface

--- +gemini3d/+vis/gemini_plot.m
+++ +gemini3d/+vis/gemini_plot.m
@@ -1,4 +1,4 @@
-function gemini_plot(varargin)
+function gemini_plot(direc, saveplot_fmt, parallel, xg, plotfun)
 %% Plot all quanities for all times in the simulation
 %
 % Parameters
@@ -6,8 +6,13 @@ function gemini_plot(varargin)
 % simulation_dir: top-level path of simulation
 % save_Format: 'png' or 'eps' to save. If empty do not save.
 
-narginchk(1,2)
+narginchk(1,5)
 
-gemini3d.vis.plotall(varargin{:})
+if nargin < 2, saveplot_fmt = {}; end
+if nargin < 3, parallel = 0; end
+if nargin < 4, xg = []; end
+if nargin < 5, plotfun = []; end
+
+gemini3d.vis.plotall(direc, saveplot_fmt, plotfun, xg, parallel)
 
 end % function
