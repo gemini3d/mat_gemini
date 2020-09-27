245223f2c96373ca8917bbe52c8fba9044c5b37d
Michael Hirsch <scivision@users.noreply.github.com>
2020-09-27 08:12:25 -0400

    datelab: vectorize

--- +gemini3d/datelab.m
+++ +gemini3d/datelab.m
@@ -2,12 +2,16 @@ function strlab = datelab(time)
 % convert gemini time format to string
 % microsecond resolution
 arguments
-  time (1,1) datetime
+  time datetime
 end
 
 day = datetime(time.Year, time.Month, time.Day);
 UTsec = seconds(time - day);
 
-strlab = datestr(day, "yyyymmdd") + "_" + num2str(UTsec, '%012.6f');
+strlab = string.empty;
+
+for i = 1:length(day)
+  strlab(i) = datestr(day(i), "yyyymmdd") + "_" + num2str(UTsec(i), '%012.6f'); %#ok<AGROW>
+end
 
 end % function
