edca5c4de3f292e71317707665a67254ce3d4f1a
Michael Hirsch <scivision@users.noreply.github.com>
2020-10-15 15:12:56 -0400

    loadframe: add same directory pwd test

--- +gemini3d/+tests/TestZData.m
+++ +gemini3d/+tests/TestZData.m
@@ -23,9 +23,11 @@ end
 methods (Test)
 
 function test_loadframe_standalone_file(tc)
-copyfile(fullfile(tc.TestData.data_path, "20130220_18000.000001.h5"), tc.TestData.outdir)
+tc.applyFixture(matlab.unittest.fixtures.WorkingFolderFixture)
 
-dat = gemini3d.loadframe(fullfile(tc.TestData.outdir, "20130220_18000.000001.h5"), "vars", "ne");
+copyfile(fullfile(tc.TestData.data_path, "20130220_18000.000001.h5"), pwd)
+
+dat = gemini3d.loadframe("20130220_18000.000001.h5", "vars", "ne");
 tc.assertEqual(dat.lxs, size(dat.ns, 1:3))
 tc.assertEqual([dat.lxs(1), dat.lxs(3)], size(dat.ne))
 tc.assertEqual(dat.time, datetime(2013,2,20,5,0,0))
