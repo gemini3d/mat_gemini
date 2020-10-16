dbde2e3caa2243c5b30431bfb613360be0df1d50
Michael Hirsch <scivision@users.noreply.github.com>
2020-10-15 22:42:08 -0400

    cmake: dedupe, parallel build

--- +gemini3d/+sys/build_gemini3d.m
+++ +gemini3d/+sys/build_gemini3d.m
@@ -9,30 +9,8 @@ if isempty(gemini_root)
   error("build_gemini3d:environment_error", "please run mat_gemini/setup.m to setup your environment")
 end
 
-exe = gemini3d.posix(exe);
-% must be absolute path for Unix-like, where cannot traverse upwards from non-existent dir
-gemini_root = gemini3d.posix(gemini3d.fileio.absolute_path(gemini_root));
-
 build_dir = fileparts(exe);
 
-if ~gemini3d.sys.cmake_atleast("3.15")
-  error("build_gemini3d:environment_error", "CMake >= 3.15 required for Gemini")
-end
-
-if ~isfolder(gemini_root)
-  error("build_gemini3d:file_not_found", "gemini_root source directory not found: " + gemini_root)
-end
-
-cfg_cmd = "cmake -B" + build_dir + " -S" + gemini_root;
-ret = system(cfg_cmd);
-if ret ~= 0
-  error("build_gemini3d:runtime_error", "Could not configure Gemini3D in " + gemini_root)
-end
-
-build_cmd = "cmake --build " + build_dir;
-ret = system(build_cmd);
-if ret ~= 0
-  error("build_gemini3d:runtime_error", "Could not build Gemini3D in " + gemini_root)
-end
+gemini3d.sys.cmake(gemini_root, build_dir)
 
 end
--- +gemini3d/+sys/cmake.m
+++ +gemini3d/+sys/cmake.m
@@ -6,20 +6,27 @@ arguments
   build_dir (1,1) string
 end
 
+% must be absolute path for Unix-like, where cannot traverse upwards from non-existent dir
+src_dir = gemini3d.posix(gemini3d.fileio.absolute_path(src_dir));
+
 ret = system('cmake --version');
 if ret ~= 0
   error('cmake:environment_error', 'CMake not found')
 end
 
+if ~isfolder(src_dir)
+  error("cmake:file_not_found", "source directory not found: " + src_dir)
+end
+
 % don't use ctest -S as it will infinite loop with MSIS build/test
 ret = system("cmake -S" + src_dir + " -B" + build_dir + " -DBUILD_TESTING:BOOL=off");
 if ret ~= 0
-  error('cmake:runtime_error', 'error configuring MSIS with CMake')
+  error('cmake:runtime_error', 'error configuring with CMake')
 end
 
-ret = system("cmake --build " + build_dir);
+ret = system("cmake --build " + build_dir + " --parallel");
 if ret ~= 0
-  error('cmake:runtime_error', 'error building MSIS with CMake')
+  error('cmake:runtime_error', 'error building with CMake')
 end
 
 end % function
