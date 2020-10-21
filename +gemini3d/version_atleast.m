c11f2f5f99f56383990808497ba7f6482b189596
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-10-21 14:58:55 -0400

    bugfix: correct version compare

--- +gemini3d/version_atleast.m
+++ +gemini3d/version_atleast.m
@@ -1,23 +1,36 @@
 function r = version_atleast(in, ref)
 % compare two string verions: major.minor.rev.patch
+% uses strings to compare so mixed number/string is OK
+%
+% parameters
+% ------------
+% in: version to examine (string)
+% ref: version to compare against (at least this version is true)
+%
+% returns
+% -------
+% r: logical
+
 arguments
     in (1,1) string
     ref (1,1) string
 end
 
 in = split(in, ' ');
-in = str2double(split(in(1), '.'));
+in = split(in(1), '.');
 
-ref = str2double(split(ref, '.'));
-
-r = false;
+ref = split(ref, '.');
 
 for i = 1:min(length(in), length(ref))
-  if in(i) < ref(i)
+  if in(i) > ref(i)
+    r = true;
+    return
+  elseif in(i) < ref(i)
+    r = false;
     return
   end
 end
 
-r = true;
+r = in(end) >= ref(end);
 
 end % function
