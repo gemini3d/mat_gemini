843e6261aaf740ab8d9694e418d68747cd33442c
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-10-23 11:32:35 -0400

    functionalize, fix bad zip file

--- +gemini3d/+fileio/download_and_extract.m
+++ +gemini3d/+fileio/download_and_extract.m
@@ -9,14 +9,33 @@ arguments
   url_ini string = string.empty
 end
 
-if isempty(url_ini)
-  url_ini = fullfile(fileparts(mfilename('fullpath')), "../+tests/gemini3d_url.ini");
-end
-
 data_dir = gemini3d.fileio.expanduser(data_dir);
 test_dir = fullfile(data_dir, "test" + test_name);
 if isfolder(test_dir)
   return
+end
+
+zipfile = download_data(test_name, data_dir, url_ini);
+
+%% extract
+try
+  unzip(zipfile, data_dir)
+catch e
+  if strcmp(e.identifier, 'MATLAB:io:archive:unzip:invalidZipFile')
+    % botched download
+    delete(zipfile)
+    zipfile = download_data(test_name, data_dir, url_ini);
+    unzip(zipfile, data_dir)
+  end
+end
+
+end % function
+
+
+function zipfile = download_data(test_name, data_dir, url_ini)
+
+if isempty(url_ini)
+  url_ini = fullfile(fileparts(mfilename('fullpath')), "../+tests/gemini3d_url.ini");
 end
 
 gemini3d.fileio.makedir(data_dir)
@@ -25,22 +44,29 @@ urls = gemini3d.vendor.ini2struct.ini2struct(url_ini);
 
 zipfile = fullfile(data_dir, "test" + test_name + ".zip");
 
-if ~isfile(zipfile) || ...
-    dir(zipfile).bytes < 10000  % missing or empty zip
-
-  if isfile(getenv("SSL_CERT_FILE"))
-    web_opts = weboptions('CertificateFilename', getenv("SSL_CERT_FILE"), 'Timeout', 15);
-  else
-    web_opts = weboptions('Timeout', 15);  % 5 seconds has nuisance timeouts
-  end
-
-  k = "url";
-  url = urls.("x" + test_name).(k);
-  websave(zipfile, url, web_opts);
+if isfile(zipfile) && dir(zipfile).bytes > 10000
+  return
 end
 
-%% md5sum check
+if isfile(getenv("SSL_CERT_FILE"))
+  web_opts = weboptions('CertificateFilename', getenv("SSL_CERT_FILE"), 'Timeout', 15);
+else
+  web_opts = weboptions('Timeout', 15);  % 5 seconds has nuisance timeouts
+end
+
+k = "url";
+url = urls.("x" + test_name).(k);
+websave(zipfile, url, web_opts);
+
+check_data(test_name, zipfile, urls)
+
+end
+
+
+function check_data(test_name, zipfile, urls)
+
 k = "md5";
+
 if isfield(urls.("x" + test_name), k)
   exp_hash = urls.("x" + test_name).(k);
   hash = gemini3d.fileio.md5sum(zipfile);
@@ -48,7 +74,5 @@ if isfield(urls.("x" + test_name), k)
     warning('%s md5 hash does not match, file may be corrupted or incorrect data', zipfile)
   end
 end
-%% extract
-unzip(zipfile, data_dir)
 
-end % function
+end
