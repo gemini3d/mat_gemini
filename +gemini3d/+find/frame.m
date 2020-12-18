b266a03582477d5d6f8f165e2aefa7d49440c302
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-12-17 23:46:37 -0500

    find.frame: nearest time

--- +gemini3d/+find/frame.m
+++ +gemini3d/+find/frame.m
@@ -17,19 +17,31 @@ for ext = suffix
   stem = stem0;
   fn = fullfile(direc, stem + ext);
   if isfile(fn)
-    break
-  end
-  % switch microsecond to one for first time step
-  i = strlength(stem);
-  stem = replaceBetween(stem, i, i, "1");
-  fn = fullfile(direc, stem + ext);
-  if isfile(fn)
-    break
+    filename = fn;
+    return
   end
 end
 
-if isfile(fn)
-  filename = fn;
-end
+%% workaround: imprecise real32 filename time
+% This will be removed when datetime-fortran is implemented
+MAX_OFFSET = seconds(1);
+%10 ms precision, allow extra accumulated tolerance
 
+pat = datestr(time, "yyyymmdd") + "_*";
+file_times = datetime.empty;
+for ext = suffix
+    files = dir(fullfile(direc, pat + ext));
+    for i = 1:length(files)
+        file_times(i) = datetime(files(i).name(1:8), "InputFormat", "yyyyMMdd") + seconds(str2double(files(i).name(10:21)));
+    end
+
+    if ~isempty(file_times)
+        [~, i] = min(abs(file_times - time));
+
+        if abs(file_times(i) - time) <= MAX_OFFSET
+            filename = fullfile(direc, files(i).name);
+            return
+        end
+    end
+end
 end % function
