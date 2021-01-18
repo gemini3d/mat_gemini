e507c859429fd5197bec2555bcd4b35a5bf4e2a4
Michael Hirsch <scivision@users.noreply.github.com>
2021-01-17 23:54:46 -0500

    remove unused code

--- +gemini3d/+sys/modify_path.m
+++ +gemini3d/+sys/modify_path.m
@@ -1,7 +1,5 @@
 function prepend = modify_path()
-% for Octave on Windows, it's necessary to prepend MinGW path
-% when running MinGW-compiled executables
-% Also, Matlab with Parallel Toolbox MPIEXEC conflicts with system MPIEXEC,
+% Matlab with Parallel Toolbox MPIEXEC conflicts with system MPIEXEC,
 % so excise from Path
 %
 % a command is then run like
@@ -12,18 +10,9 @@ prepend = "";
 
 if ~ispc, return, end
 
-% if isoctave && ~isempty(getenv('MINGWROOT'))
-%
-%   syspath = [getenv('MINGWROOT'), pathsep, getenv('PATH')];
-%   % setenv('PATH', syspath)  % does not help
-%   prepend = ['set PATH=', syspath, ' && '];
-%
-% else
-
 path_new = gemini3d.sys.mpi_path();
 
 % setenv('PATH', path_new{1}) % does not help
 prepend = "set PATH=" + path_new + " && ";
-
 
 end % function
