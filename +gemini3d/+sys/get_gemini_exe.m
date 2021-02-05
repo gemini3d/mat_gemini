bf9dd6709a0c8853ec82e17ed07f58f763477206
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2021-02-05 16:43:30 -0500

    use Fortran-executable gemini3d.run

--- +gemini3d/+sys/get_gemini_exe.m
+++ +gemini3d/+sys/get_gemini_exe.m
@@ -3,6 +3,8 @@ arguments
   exe string = string.empty
 end
 
+runner_name = "gemini.bin";
+
 exe = gemini3d.sys.exe_name(exe);
 
 if isempty(exe) || ~isfile(exe)
@@ -13,20 +15,20 @@ if isempty(exe) || ~isfile(exe)
     srcdir = gemini3d.fileio.expanduser(getenv("GEMINI_ROOT"));
   end
   bindir = fullfile(srcdir, "build");
-  exe = gemini3d.sys.exe_name(fullfile(bindir, "gemini.bin"));
+  exe = gemini3d.sys.exe_name(fullfile(bindir, runner_name));
 
   if ~isfile(exe)
-    gemini3d.sys.cmake(srcdir, bindir, "gemini.bin");
-    exe = gemini3d.sys.exe_name(fullfile(bindir, "gemini.bin"));
+    gemini3d.sys.cmake(srcdir, bindir, runner_name);
+    exe = gemini3d.sys.exe_name(fullfile(bindir, runner_name));
   end
 end
 assert(isfile(exe), 'failed to build ' + exe)
-%% sanity check gemini.bin executable
+%% sanity check Gemini executable
 prepend = gemini3d.sys.modify_path();
 [ret, msg] = system(prepend + " " + exe);
 
 if ret ~= 0
-  error("get_gemini_exe:runtime_error", "problem with Gemini.bin executable: %s  error: %s", exe, msg)
+  error("get_gemini_exe:runtime_error", "problem with Gemini executable: %s  error: %s", exe, msg)
 end
 
 end % function
--- +gemini3d/run.m
+++ +gemini3d/run.m
@@ -14,6 +14,7 @@ arguments
   opts.ssl_verify string = string.empty
   opts.file_format string = string.empty
   opts.dryrun (1,1) logical = false
+  opts.fortran_runner (1,1) logical = false
 end
 
 %% ensure all paths are OK
@@ -21,8 +22,10 @@ cwd = fileparts(mfilename('fullpath'));
 run(fullfile(cwd, '../setup.m'))
 %% get gemini.bin executable
 gemini_exe = gemini3d.sys.get_gemini_exe(opts.gemini_exe);
+
 %% ensure mpiexec is available
 mpiexec_ok = gemini3d.sys.check_mpiexec(opts.mpiexec, gemini_exe);
+
 %% check if model needs to be setup
 cfg = setup_if_needed(opts, outdir, config_path);
 %% check that no existing simulation is in output directory
@@ -33,7 +36,7 @@ if ~isfield(cfg, 'mcadence') || cfg.mcadence < 0
 end
 
 %% assemble run command
-cmd = create_run(cfg, opts.mpiexec, gemini_exe, mpiexec_ok);
+cmd = create_run(cfg, opts.mpiexec, gemini_exe, mpiexec_ok, opts.fortran_runner);
 
 if opts.dryrun
   return
@@ -42,7 +45,11 @@ end
 gemini3d.write.meta(gemini3d.git_revision(fileparts(gemini_exe)), ...
                       fullfile(cfg.outdir, "setup_meta.nml"), 'setup_gemini')
 
-ret = exe_run(cmd, mpiexec_ok);
+if opts.fortran_runner
+  ret = system(cmd);
+else
+  ret = exe_run(cmd, mpiexec_ok);
+end
 if ret ~= 0
   error('run:runtime_error', 'Gemini run failed, error code %d', ret)
 end
@@ -87,23 +94,34 @@ end
 end % function
 
 
-function cmd = create_run(cfg, mpiexec, gemini_exe, mpiexec_ok)
+function cmd = create_run(cfg, mpiexec, gemini_exe, mpiexec_ok, fortran)
 arguments
   cfg (1,1) struct
   mpiexec (1,1) string
   gemini_exe (1,1) string
   mpiexec_ok (1,1) logical
+  fortran (1,1) logical
 end
 
-np = gemini3d.sys.get_mpi_count(fullfile(cfg.outdir, cfg.indat_size));
-
-%% mpiexec if available
 cmd = gemini_exe + " " + cfg.outdir;
-if mpiexec_ok
-  cmd = sprintf("%s -n %d %s", mpiexec, np, cmd);
+
+if fortran
+  parent = fileparts(gemini_exe);
+  gembin = fullfile(parent, "gemini.bin");
+  if ispc
+    gembin = gembin + ".exe";
+  end
+  cmd = cmd + " -gemexe " + gembin;
 else
-  disp("MPIexec not available, falling back to single CPU core execution.")
+  %% mpiexec if available
+  if mpiexec_ok
+    np = gemini3d.sys.get_mpi_count(fullfile(cfg.outdir, cfg.indat_size));
+    cmd = sprintf("%s -n %d %s", mpiexec, np, cmd);
+  else
+    disp("MPIexec not available, falling back to single CPU core execution.")
+  end
 end
+
 
 disp(cmd)
 
