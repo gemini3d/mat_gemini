78ccfea85479c5d1ef962d36615d053b9e2292d5
Matthew Zettergren <mattzett@gmail.com>
2021-02-17 22:19:30 -0500

    adjustments for 2D open dipole

--- +gemini3d/+model/Efield_BCs.m
+++ +gemini3d/+model/Efield_BCs.m
@@ -15,15 +15,43 @@ lx1 = xg.lx(1);
 lx2 = xg.lx(2);
 lx3 = xg.lx(3);
 
-%% CREATE ELECTRIC FIELD DATASET
+
+%% For current density boundary conditions we need to determin top v bottom of the grid
+if (xg.alt(1,1,1)>xg.alt(2,1,1))
+    gridflag=1;    % inverted
+    disp(" Efield_BCs:  Detected an inverted grid...");
+else
+    gridflag=2;    % non-inverted or closed
+    disp(" Efield_BCs:  Detected a non-inverted grid...");
+end %if
+
+%% determine what type of grid (cartesian or dipole) we are dealing with
+if (any(xg.h1>1.01))
+    flagdip=true;
+    disp(' Efield_BCs:  Dipole grid detected...');
+else
+    flagdip=false;
+    disp(' Efield_BCs:  Cartesian grid detected...');
+end %if
+
+
+%% number of points for input data
 E.llon=100;
 E.llat=100;
-% NOTE: cartesian-specific code
-if lx2 == 1
-  E.llon = 1;
-elseif lx3 == 1
-  E.llat = 1;
-end
+if (flagdip)
+    if lx3 == 1
+        E.llon=1;
+    elseif lx2==1
+        E.llat=1;        
+    end %if
+else
+    if lx2 == 1
+        E.llon = 1;
+    elseif lx3 == 1
+        E.llat = 1;
+    end
+end %if
+
 thetamin = min(xg.theta(:));
 thetamax = max(xg.theta(:));
 mlatmin = 90-thetamax*180/pi;
@@ -42,23 +70,43 @@ E.mlatmean = mean(E.mlat);
 
 %% WIDTH OF THE DISTURBANCE
 if isfield(p, 'Efield_latwidth')
-  [E.mlatsig, E.sigx3] = Esigma(p.Efield_latwidth, mlatmax, mlatmin, xg.x3);
+    if (flagdip)
+        [E.mlatsig, E.sigx2] = Esigma(p.Efield_latwidth, mlatmax, mlatmin, xg.x2);
+    else
+        [E.mlatsig, E.sigx3] = Esigma(p.Efield_latwidth, mlatmax, mlatmin, xg.x3);
+    end %if
 end
 if isfield(p, 'Efield_lonwidth')
-   [E.mlonsig, E.sigx2] = Esigma(p.Efield_lonwidth, mlonmax, mlonmin, xg.x2);
+    if (flagdip)
+        [E.mlonsig, E.sigx3] = Esigma(p.Efield_lonwidth, mlonmax, mlonmin, xg.x3);
+    else
+        [E.mlonsig, E.sigx2] = Esigma(p.Efield_lonwidth, mlonmax, mlonmin, xg.x2);
+    end %if
 end
 if isfield(p, 'Efield_fracwidth')
   warning('Efield_fracwidth is deprecated. Please use Efield_lonwidth or Efield_latwidth')
   if E.llat ~= 1
-    [E.mlatsig, E.sigx3] = Esigma(p.Efield_fracwidth, mlatmax, mlatmin, xg.x3);
+      if (flagdip)
+          [E.mlatsig, E.sigx2] = Esigma(p.Efield_fracwidth, mlatmax, mlatmin, xg.x2);
+      else
+          [E.mlatsig, E.sigx3] = Esigma(p.Efield_fracwidth, mlatmax, mlatmin, xg.x3);
+      end %if
   end
   if E.llon ~= 1
-    [E.mlonsig, E.sigx2] = Esigma(p.Efield_fracwidth, mlonmax, mlonmin, xg.x2);
+      if (flagdip)
+          [E.mlonsig, E.sigx3] = Esigma(p.Efield_fracwidth, mlonmax, mlonmin, xg.x3);
+      else
+          [E.mlonsig, E.sigx2] = Esigma(p.Efield_fracwidth, mlonmax, mlonmin, xg.x2);
+      end %if
   end
 end
+
+
 %% TIME VARIABLE (SECONDS FROM SIMULATION BEGINNING)
 E.times = p.times(1):seconds(p.dtE0):p.times(end);
 Nt = length(E.times);
+
+
 %% CREATE DATA FOR BACKGROUND ELECTRIC FIELDS
 if isfield(p, 'Exit')
   E.Exit = p.Exit * ones(E.llon, E.llat, Nt);
@@ -71,6 +119,7 @@ else
   E.Eyit = zeros(E.llon, E.llat, Nt);
 end
 
+
 %% CREATE DATA FOR BOUNDARY CONDITIONS FOR POTENTIAL SOLUTION
 % if 0 data is interpreted as FAC, else we interpret it as potential
 E.flagdirich=zeros(Nt,1);    %in principle can have different boundary types for different time steps...
@@ -82,21 +131,10 @@ E.Vmaxx2ist = zeros(E.llat, Nt);
 E.Vminx3ist = zeros(E.llon, Nt);
 E.Vmaxx3ist = zeros(E.llon, Nt);
 
-
-%% For current density boundary conditions we need to determin top v bottom of the grid
-if (xg.alt(1,1,1)>xg.alt(2,1,1))
-    gridflag=1;    % inverted
-    disp(" Efield_BCs:  Detected an inverted grid...");
-else
-    gridflag=2;    % non-inverted or closed
-    disp(" Efield_BCs:  Detected a non-inverted grid...");
-end %if
-
-
 %% synthesize feature
 if isfield(p, 'Etarg')
   E.Etarg = p.Etarg;
-  E = Efield_target(E, xg, lx1, lx2, lx3, Nt);
+  E = Efield_target(E, xg, lx1, lx2, lx3, Nt, gridflag,flagdip);
 elseif isfield(p, 'Jtarg')
   E.Jtarg = p.Jtarg;
   E = Jcurrent_target(E, Nt, gridflag);
@@ -104,11 +142,11 @@ else
   % background only, pass
 end
 
+
 %% SAVE THESE DATA TO APPROPRIATE FILES
 % LEAVE THE SPATIAL AND TEMPORAL INTERPOLATION TO THE
 % FORTRAN CODE IN CASE DIFFERENT GRIDS NEED TO BE TRIED.
 % THE EFIELD DATA DO NOT TYPICALLY NEED TO BE SMOOTHED.
-
 gemini3d.write.Efield(E, dir_out, p.file_format)
 
 end % function
@@ -132,28 +170,46 @@ end
 end % function
 
 
-function E = Efield_target(E, xg, lx1, lx2, lx3, Nt)
+function E = Efield_target(E, xg, lx1, lx2, lx3, Nt,gridflag,flagdip)
 
 % Set the top boundary shape (potential) and potential solve type flag
 
 %% create feature defined by Efield
 % NOTE: h2, h3 have ghost cells, so we use lx1 instead of "end" to index
 % pk is a scalar.
-if lx3 == 1 % east-west
-  S = E.Etarg * E.sigx2 .* xg.h2(lx1, floor(lx2/2), 1) .* sqrt(pi)./2;
-  taper = erf((E.MLON - E.mlonmean) / E.mlonsig);
-elseif lx2 == 1 % north-south
-  S = E.Etarg * E.sigx3 .* xg.h3(lx1, 1, floor(lx3/2)) .* sqrt(pi)./2;
-  taper = erf((E.MLAT - E.mlatmean) / E.mlatsig);
-else % 3D
-  S = E.Etarg * E.sigx2 .* xg.h2(lx1, floor(lx2/2), 1) .* sqrt(pi)./2;
-  taper = erf((E.MLON - E.mlonmean) / E.mlonsig) .* erf((E.MLAT - E.mlatmean) / E.mlatsig);
-end
+
+if (flagdip)
+    if lx3 == 1 % meridional slice
+        S = E.Etarg * E.sigx2 .* xg.h2(lx1, floor(lx2/2), 1) .* sqrt(pi)./2;
+        taper = erf((E.MLAT - E.mlatmean) / E.mlatsig);
+    elseif (lx2>1 & lx3>1)% 3D
+        S = E.Etarg * E.sigx2 .* xg.h2(lx1, floor(lx2/2), 1) .* sqrt(pi)./2;
+        taper = erf((E.MLON - E.mlonmean) / E.mlonsig) .* erf((E.MLAT - E.mlatmean) / E.mlatsig);
+    else
+        error(' Efield_target:  you appear to be making a zonal ribbon grid, which is not yet supported');
+    end
+else
+    if lx3 == 1 % east-west
+        S = E.Etarg * E.sigx2 .* xg.h2(lx1, floor(lx2/2), 1) .* sqrt(pi)./2;
+        taper = erf((E.MLON - E.mlonmean) / E.mlonsig);
+    elseif lx2 == 1 % north-south
+        S = E.Etarg * E.sigx3 .* xg.h3(lx1, 1, floor(lx3/2)) .* sqrt(pi)./2;
+        taper = erf((E.MLAT - E.mlatmean) / E.mlatsig);
+    else % 3D
+        S = E.Etarg * E.sigx2 .* xg.h2(lx1, floor(lx2/2), 1) .* sqrt(pi)./2;
+        taper = erf((E.MLON - E.mlonmean) / E.mlonsig) .* erf((E.MLAT - E.mlatmean) / E.mlatsig);
+    end
+end %if
 
 % x2ctr = 1/2*(xg.x2(lx2) + xg.x2(1));
 for i = 1:Nt
   E.flagdirich(i)=1;
-  E.Vmaxx1it(:,:,i) = S .* taper;
+  
+  if (gridflag==1)
+      E.Vminx1it(:,:,i) = S .* taper;
+  else
+      E.Vmaxx1it(:,:,i) = S .* taper;
+  end %if
 end
 
 end % function
