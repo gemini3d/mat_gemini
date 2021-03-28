fad746b0c2b866b9ace7062929a269252b36331a
Matthew Zettergren <zettergm@Matthews-Mac-Pro.local>
2021-03-28 09:43:19 -0400

    checking too aggressive

--- +gemini3d/+model/private/check_density.m
+++ +gemini3d/+model/private/check_density.m
@@ -2,6 +2,6 @@ function check_density(n)
 
 mustBeFinite(n)
 mustBeNonnegative(n)
-mustBeGreaterThan(max(n), 1e4)
+%mustBeGreaterThan(max(n), 1e4)
 
 end
--- +gemini3d/+model/private/check_temperature.m
+++ +gemini3d/+model/private/check_temperature.m
@@ -2,6 +2,6 @@ function check_temperature(T)
 
 mustBeFinite(T)
 mustBeNonnegative(T)
-mustBeGreaterThan(max(T), 500)
+%mustBeGreaterThan(max(T), 500)
 
 end
