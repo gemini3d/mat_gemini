11dea4184a5937db40b101d776f1dcb806c32332
Michael Hirsch <scivision@users.noreply.github.com>
2021-06-21 11:12:23 -0400

    read.grid.raw: dedupe with for loop

--- +gemini3d/+read/grid.m
+++ +gemini3d/+read/grid.m
@@ -136,28 +136,24 @@ tmp=fread(fid,ltmp, freal);
 xgf.h3x3i=reshape(tmp,tmpsize);
 
 %gravity, geographic coordinates, magnetic field strength? unit vectors?
-tmp=fread(fid,lgrid, freal);    %gravitational field components
-xgf.gx1=reshape(tmp,gridsize);
-tmp=fread(fid,lgrid, freal);
-xgf.gx2=reshape(tmp,gridsize);
-tmp=fread(fid,lgrid, freal);
-xgf.gx3=reshape(tmp,gridsize);
+%gravitational field components
+for k = ["gx1", "gx2", "gx3"]
+  xgf.(k)=reshape(fread(fid,lgrid, freal),gridsize);
+end
 
-tmp=fread(fid,lgrid, freal);    %geographic coordinates
-xgf.alt=reshape(tmp,gridsize);
-tmp=fread(fid,lgrid, freal);
-xgf.glat=reshape(tmp,gridsize);
-tmp=fread(fid,lgrid, freal);
-xgf.glon=reshape(tmp,gridsize);
+%geographic coordinates
+for k = ["alt", "glat", "glon"]
+  xgf.(k)=reshape(fread(fid,lgrid, freal),gridsize);
+end
 
-tmp=fread(fid,lgrid, freal);    %magnetic field strength
-xgf.Bmag=reshape(tmp,gridsize);
+%magnetic field strength
+xgf.Bmag=reshape(fread(fid,lgrid, freal),gridsize);
 
-tmp=fread(fid,lx2*lx3, freal);    %magnetic field inclination - only one value for each field line
-xgf.I=reshape(tmp,[lx2,lx3]);
+%magnetic field inclination - only one value for each field line
+xgf.I=reshape(fread(fid,lx2*lx3, freal),[lx2,lx3]);
 
-tmp=fread(fid,lgrid, freal);    %points not to be solved
-xgf.nullpts=reshape(tmp,gridsize);
+%points not to be solved
+xgf.nullpts=reshape(fread(fid,lgrid, freal),gridsize);
 
 
 %STUFF PAST THIS POINT ISN'T USED IN FORTRAN CODE BUT INCLUDED IN THE
@@ -165,40 +161,22 @@ xgf.nullpts=reshape(tmp,gridsize);
 if ~feof(fid)
   tmpsize=[lx1,lx2,lx3,3];
   ltmp=prod(tmpsize);
-  tmp=fread(fid,ltmp, freal);   %4D unit vectors (in cartesian components)
 
-  if (feof(fid))    %for whatever reason, we sometimes don't hit eof until after first unit vector read...
-    return;   %lazy as hell
+  %4D unit vectors (in cartesian components)
+  for k = ["e1", "e2", "e3", "er", "etheta", "ephi"]
+    xgf.(k)=reshape(fread(fid,ltmp, freal), tmpsize);
+    if feof(fid), return, end
   end
 
-  xgf.e1=reshape(tmp,tmpsize);
-  tmp=fread(fid,ltmp, freal);
-  xgf.e2=reshape(tmp,tmpsize);
-  tmp=fread(fid,ltmp, freal);
-  xgf.e3=reshape(tmp,tmpsize);
+  %spherical coordinates
+  for k = ["r", "theta", "phi"]
+    xgf.(k)=reshape(fread(fid,lgrid, freal),gridsize);
+  end
 
-  tmpsize=[lx1,lx2,lx3,3];
-  ltmp=prod(tmpsize);
-  tmp=fread(fid,ltmp, freal);
-  xgf.er=reshape(tmp,tmpsize);
-  tmp=fread(fid,ltmp, freal);
-  xgf.etheta=reshape(tmp,tmpsize);
-  tmp=fread(fid,ltmp, freal);
-  xgf.ephi=reshape(tmp,tmpsize);
-
-  tmp=fread(fid,lgrid, freal);    %spherical coordinates
-  xgf.r=reshape(tmp,gridsize);
-  tmp=fread(fid,lgrid, freal);
-  xgf.theta=reshape(tmp,gridsize);
-  tmp=fread(fid,lgrid, freal);
-  xgf.phi=reshape(tmp,gridsize);
-
-  tmp=fread(fid,lgrid, freal);     %cartesian coordinates
-  xgf.x=reshape(tmp,gridsize);
-  tmp=fread(fid,lgrid, freal);
-  xgf.y=reshape(tmp,gridsize);
-  tmp=fread(fid,lgrid, freal);
-  xgf.z=reshape(tmp,gridsize);
+  %cartesian coordinates
+  for k = ["x", "y", "z"]
+    xgf.(k)=reshape(fread(fid,lgrid, freal),gridsize);
+  end
 end
 
 fclose(fid);
