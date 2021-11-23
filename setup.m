5052e17828506fde0a24e511788647d510c95f53
Michael Hirsch <scivision@users.noreply.github.com>
2021-11-23 13:06:49 -0500

    stdlib: improved test coverage

--- setup.m
+++ setup.m
@@ -5,7 +5,7 @@ assert(~verLessThan('matlab', '9.9'), 'Matlab >= R2020b is required')
 
 cwd = fileparts(mfilename('fullpath'));
 addpath(cwd)
-
+setenv("MATGEMINI", cwd)
 %% ensure matlab-stdlib is present
 stdlib_dir = fullfile(cwd, "matlab-stdlib");
 assert(system("git submodule update --init") == 0, ...
