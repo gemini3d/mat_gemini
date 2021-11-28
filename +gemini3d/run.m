dd5efacb2db9b4009eccc77f0c1a846430e60e17
Michael Hirsch <scivision@users.noreply.github.com>
2021-11-28 12:22:09 -0500

    run: import stdlib

--- +gemini3d/run.m
+++ +gemini3d/run.m
@@ -16,6 +16,8 @@ arguments
   opts.dryrun (1,1) logical = false
 end
 
+import stdlib.sys.subprocess_run
+
 %% find or build gemini.bin executable
 gemini_exe = gemini3d.sys.get_gemini_exe(opts.gemini_exe);
 if isempty(gemini_exe)
@@ -28,25 +30,15 @@ end
 assert(~isempty(gemini_exe), "Gemini3D executable not found")
 %% check if model needs to be setup
 cfg = setup_if_needed(opts, outdir, config_path);
-%% check that no existing simulation is in output directory
-% unless this is a milestone restart
-if ~isfield(cfg, 'mcadence') || cfg.mcadence < 0
-  try %#ok<TRYNC>
-    gemini3d.find.frame(outdir, cfg.times(1));
-    error("gemini3d:run:FileExists", "a fresh simulation should not have data in output directory: %s", outdir)
-  end
-end
-
 %% check MPIexec
 mpiexec = gemini3d.sys.check_mpiexec(opts.mpiexec, gemini_exe);
-
 %% assemble run command
 cmd = [gemini_exe, cfg.outdir];
 if ~isempty(mpiexec)
   cmd = [cmd, "-mpiexec", '"' + mpiexec + '"'];
 end
 disp("dryrun: " + join(cmd, " "))
-[ret, msg] = stdlib.sys.subprocess_run([cmd, "-dryrun"]);
+[ret, msg] = subprocess_run([cmd, "-dryrun"]);
 if ret ~=0
   error("gemini3d:run:RuntimeError", "Gemini dryrun failed %s", msg)
 end
@@ -58,7 +50,7 @@ end
 gemini3d.write.meta(fullfile(cfg.outdir, "setup_run.json"), gemini3d.git_revision(fileparts(gemini_exe)), cfg)
 
 disp("run: " + join(cmd, " "))
-ret = stdlib.sys.subprocess_run(cmd);
+ret = subprocess_run(cmd);
 assert(ret == 0, 'Gemini run failed, error code %d', ret)
 
 end % function
