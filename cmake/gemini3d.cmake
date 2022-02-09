997a85f7840a00d50de9fc7e70a98a7b27bf1712
Michael Hirsch <scivision@users.noreply.github.com>
2022-02-09 15:40:51 -0500

    cmake: require MPI

--- cmake/gemini3d.cmake
+++ cmake/gemini3d.cmake
@@ -1,31 +1,23 @@
 include(ExternalProject)
 
-# Can run Gemini3D without MPI, but it's a lot slower
-find_package(MPI COMPONENTS C Fortran)
-if(MPI_FOUND)
-  set(mpi on)
-else()
-  set(mpi off)
-endif()
 
+cmake_path(SET GEMINI_ROOT ${PROJECT_BINARY_DIR})
 
-set(GEMINI_ROOT ${PROJECT_BINARY_DIR})
-
-set(gemini3d_args
+set(gemini_args
 -DBUILD_TESTING:BOOL=off
--Dmpi:BOOL=${mpi}
+-Dmpi:BOOL=on
 -Dmsis2:BOOL=on
 -Dglow:BOOL=on
 -Dhdf5:BOOL=on
 -Dhwm14:BOOL=${hwm14}
 -Dnetcdf:BOOL=off
---install-prefix=${GEMINI_ROOT}
+-DCMAKE_INSTALL_PREFIX=${GEMINI_ROOT}
 )
 
 ExternalProject_Add(GEMINI3D_RELEASE
 GIT_REPOSITORY ${gemini3d_url}
 GIT_TAG ${gemini3d_tag}
-CMAKE_ARGS ${gemini3d_args} -DCMAKE_BUILD_TYPE=Release
+CMAKE_ARGS ${gemini_args} -DCMAKE_BUILD_TYPE=Release
 CMAKE_GENERATOR ${EXTPROJ_GENERATOR}
 INACTIVITY_TIMEOUT 15
 CONFIGURE_HANDLED_BY_BUILD true
