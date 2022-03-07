8eb8e6a2c2a6a6c8254b1ab64762d002d02401ca
Michael Hirsch <scivision@users.noreply.github.com>
2022-03-07 09:41:44 -0500

    get_exe: also search env var CMAKE_PREFIX_PATH

--- +gemini3d/+sys/get_gemini_exe.m
+++ +gemini3d/+sys/get_gemini_exe.m
@@ -2,14 +2,14 @@ function exe = get_gemini_exe(name)
 arguments
   name (1,1) string {mustBeNonzeroLengthText}
 end
+%% Find an executable from the Gemini3D program set, using paths common to Gemini3D
 
 import stdlib.fileio.which
 import stdlib.fileio.absolute_path
 
 
-
-paths = [fullfile(gemini3d.root(), ".."), string(getenv("GEMINI_ROOT"))];
-bindirs = ["build", ".", "bin", "build/bin", "build/Release", "build/RelWithDebInfo", "build/Debug"];
+paths = [fullfile(gemini3d.root(), ".."), string(getenv("CMAKE_PREFIX_PATH")), string(getenv("GEMINI_ROOT"))];
+bindirs = [".", "bin", "build", "build/bin", "build/Release", "build/RelWithDebInfo", "build/Debug"];
 
 for p = paths
   for b = bindirs
