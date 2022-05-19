1f308bf5878da4d06e9e5ef6e4528ef19994e42d
Michael Hirsch <scivision@users.noreply.github.com>
2022-05-19 17:01:42 -0400

    remove unused code

--- +gemini3d/+sys/get_cpu_count.m
+++ +gemini3d/+sys/get_cpu_count.m
@@ -1,17 +1,13 @@
 function N = get_cpu_count()
 %% get apparent number of CPU cores
 
-if isoctave
-  N = idivide(nproc, 2);  % assume hyperthreading
-else
-  N = maxNumCompThreads;
-  if N < 2  % happens on some HPC
-    N = feature('NumCores');
-  end
-  if N < 2 && usejava('jvm')
-    % assume hyperthreading
-    N = java.lang.Runtime.getRuntime().availableProcessors / 2;
-  end
+N = maxNumCompThreads;
+if N < 2  % happens on some HPC
+  N = feature('NumCores');
+end
+if N < 2 && usejava('jvm')
+  % assume hyperthreading
+  N = java.lang.Runtime.getRuntime().availableProcessors / 2;
 end
 
 N = max(N, 1);
