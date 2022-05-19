750a7af2efef121a12d8dc4135899fbc690e7d77
Michael Hirsch <scivision@users.noreply.github.com>
2022-05-19 16:16:18 -0400

    cmake.m, setup_gemini3d.m: better install behavior

--- +gemini3d/+sys/cmake.m
+++ +gemini3d/+sys/cmake.m
@@ -1,9 +1,10 @@
-function cmake(src_dir, build_dir, cmake_args)
+function cmake(src_dir, build_dir, install_dir, cmake_args)
 % build program using CMake and default generator
 % specify CMake generator: set environment variable CMAKE_GENERATOR
 arguments
   src_dir (1,1) string
   build_dir (1,1) string
+  install_dir string {mustBeScalarOrEmpty} = string.empty
   cmake_args string {mustBeScalarOrEmpty} = string.empty
 end
 
@@ -22,16 +23,30 @@ assert(system("cmake --version") == 0, 'CMake not found')
 
 %% configure
 % don't use ctest -S as it will infinite loop with MSIS build/test
-cmd = "cmake -S" + src_dir + " -B" + build_dir + " -DBUILD_TESTING:BOOL=off -DCMAKE_INSTALL_PREFIX:PATH=" + build_dir;
+cmd = "cmake -S" + src_dir + " -B" + build_dir + " -DBUILD_TESTING:BOOL=off";
 if ~isempty(cmake_args)
   cmd = cmd + " " + cmake_args;
 end
-assert(system(cmd) == 0, "error configuring %s with CMake", src_dir)
+if ~isempty(install_dir)
+  cmd = cmd + " -DCMAKE_INSTALL_PREFIX:PATH=" + build_dir;
+end
+if system(cmd) ~= 0
+  error("CMake configure %s", src_dir)
+end
 
 %% build
 cmd = "cmake --build " + build_dir + " --parallel";
+if system(cmd) ~= 0
+  error('CMake build %s', build_dir)
+end
 
-ret = system(cmd);
-assert(ret == 0, 'error building with CMake in %s', build_dir)
+%% install
+if ~isempty(install_dir)
+  cmd = "cmake --install " + build_dir;
+
+  if system(cmd) ~= 0
+    error('CMake install %s', build_dir)
+  end
+end
 
 end % function
--- setup_gemini3d.m
+++ setup_gemini3d.m
@@ -1,9 +1,9 @@
-function setup_gemini3d(prefix)
+function setup_gemini3d(install_prefix)
 %% setup Gemini3D (build with CMake)
 % this is run automatically by functions that need it like gemini3d.run and gemini3d.model.setup
 % however if you wish you can run it manually to setup gemini3d
 arguments
-  prefix string {mustBeScalarOrEmpty} = string.empty
+  install_prefix string {mustBeScalarOrEmpty} = string.empty
 end
 
 import gemini3d.sys.cmake
@@ -12,13 +12,14 @@ import gemini3d.sys.cmake
 cwd = fullfile(fileparts(mfilename('fullpath')));
 run(fullfile(cwd, 'setup.m'))
 
-cmake_args = string.empty;
-if ~isempty(prefix)
-  cmake_args = "-DCMAKE_PREFIX_PATH:PATH=" + prefix;
+cmake(cwd, fullfile(cwd, "build"), install_prefix);
+
+if isempty(install_prefix)
+  setenv("GEMINI_ROOT", cwd)
+else
+  setenv("GEMINI_ROOT", install_prefix)
 end
 
-cmake(cwd, fullfile(cwd, "build"), cmake_args);
-
-setenv("GEMINI_ROOT", cwd)
+disp("Gemini3D installed to: " + getenv("GEMINI_ROOT"))
 
 end
