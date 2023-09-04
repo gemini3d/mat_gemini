6d7d731da4b89605dbdd8d0c206f4dfddf58d3ee
scivision <scivision@users.noreply.github.com>
2023-09-04 18:33:19 -0400

    cmake/buildtool: use buildtool test and verify tests ran

--- buildfile.m
+++ buildfile.m
@@ -19,6 +19,14 @@ exe = gemini3d.find.gemini_exe("msis_setup");
 assert(~isempty(exe), "need to setup Gemini3D and/or set environment variable GEMINI_ROOT")
 end
 
-function testTask(~)
-assertSuccess(runtests('gemini3d.test'))
+function testTask(~, test_name)
+arguments
+  ~
+  test_name (1,1) string = "*"
+end
+
+r = runtests('gemini3d.test', Name="gemini3d.test." + test_name);
+
+assert(~isempty(r))
+assertSuccess(r)
 end
