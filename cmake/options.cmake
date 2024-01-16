991fcd0d2b3b694db4cc16bd37ceb7ddaca56d77
scivision <scivision@users.noreply.github.com>
2024-01-16 13:46:12 -0500

    cmake: show ninja progress, TLS_VERIFY option

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -4,9 +4,4 @@ option(msis2 "use MSIS2 model" on)
 
 set_directory_properties(PROPERTIES EP_UPDATE_DISCONNECTED true)
 
-set(CMAKE_TLS_VERIFY true)
-
-# --- auto-ignore build directory
-if(NOT EXISTS ${PROJECT_BINARY_DIR}/.gitignore)
-  file(WRITE ${PROJECT_BINARY_DIR}/.gitignore "*")
-endif()
+option(CMAKE_TLS_VERIFY "Verify TLS certificates when downloading dependencies" on)
