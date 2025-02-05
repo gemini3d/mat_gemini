4ab9fdf4020481ba6eb414cb49db38547db47ddb
scivision <scivision@users.noreply.github.com>
2025-02-05 12:20:16 -0500

    move extract_zstd to gemini3d

--- +gemini3d/+fileio/extract_data.m
+++ +gemini3d/+fileio/extract_data.m
@@ -12,6 +12,6 @@ switch arc_type
   case ".zip", unzip(archive, data_dir)
   % old zip files had vestigial folder of same name instead of just files
   case ".tar", untar(archive, odir)
-  case {".zst", ".zstd"}, stdlib.extract_zstd(archive, odir)
+  case {".zst", ".zstd"}, gemini3d.extract_zstd(archive, odir)
   otherwise, error("gemini3d:fileio:extract_data:ValueError", "unknown reference archive type: " + arc_type)
 end
