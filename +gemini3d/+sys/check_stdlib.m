d2ba151250e64e62cb0096ed190975c2df80176d
scivision <scivision@users.noreply.github.com>
2025-03-10 12:30:05 -0400

    auto Git submodule update if would have errored

--- +gemini3d/+sys/check_stdlib.m
+++ +gemini3d/+sys/check_stdlib.m
@@ -16,11 +16,8 @@ if ~isempty(p)
 end
 
 if isempty(p) || ~any(contains(p.packages, 'stdlib'))
-  error("gemini3d:sys:check_stdlib:environment_error", ...
-    "Run this command from in Terminal, then run mat_gemini/setup.m script again:\n%s\n%s\n%s", ...
-    "git -C " + r + " submodule update --init", ...
-    "if mat_gemini/setup.m still doesn't work, perhaps try recloning mat_gemini like:", ...
-    "git clone --recurse-submodules https://github.com/gemini3d/mat_gemini")
+  ret = system("git -C " + r + " submodule update --init");
+  assert(ret == 0, "could not Git submodule update matlab-stdlib in " + r)
 end
 
 if ~contains(path(), r)
