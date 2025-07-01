ab96df19343e92beac5569120b5263662c45a1f8
Michael Hirsch <scivision@users.noreply.github.com>
2025-07-01 04:48:02 -0400

    improve test coverage

--- test/TestGrid.m
+++ test/TestGrid.m
@@ -16,8 +16,11 @@ methods (Test)
 
 function test_dipole_grid(tc)
 
+wd = tc.createTemporaryFolder();
+
 parm = struct(lq=4, lp=6, lphi=1, dtheta=7.5, dphi=12, altmin=80e3, ...
-  gridflag=1, glon=143.4, glat=42.45);
+  gridflag=1, glon=143.4, glat=42.45, ...
+  indat_grid=fullfile(wd, "inputs/simgrid.h5"), indat_size=fullfile(wd, "inputs/simsize.h5"));
 
 xg = gemini3d.grid.tilted_dipole(parm);
 
@@ -27,6 +30,8 @@ tc.verifyEqual(xg.e1(1,1,1,1), -0.847576545732110, RelTol=1e-6)
 
 tc.verifyEqual(xg.glonctr, 139.56776327, RelTol=1e-6)
 tc.verifyEqual(xg.glatctr, 9.51369548, RelTol=1e-6)
+
+gemini3d.write.grid(parm, xg)
 
 end
 
--- test/TestProject.m
+++ test/TestProject.m
@@ -6,7 +6,9 @@ properties (TestParameter)
 end
 
 properties
-  TestData
+  cwd = fileparts(mfilename('fullpath'))
+  ref_dir
+  outdir
 end
 
 methods(TestClassSetup)
@@ -31,14 +33,11 @@ function setup_env(tc)
 tc.assumeFalse(isMATLABReleaseOlderThan('R2023a'))
 import matlab.unittest.fixtures.EnvironmentVariableFixture
 
-cwd = fileparts(mfilename('fullpath'));
+tc.ref_dir = fullfile(tc.cwd, "data");
 
-tc.TestData.cwd = cwd;
-tc.TestData.ref_dir = fullfile(cwd, "data");
+tc.outdir = tc.createTemporaryFolder();
 
-tc.TestData.outdir = tc.createTemporaryFolder();
-
-fixture = EnvironmentVariableFixture("GEMINI_CIROOT", tc.TestData.outdir);
+fixture = EnvironmentVariableFixture("GEMINI_CIROOT", tc.outdir);
 tc.applyFixture(fixture)
 
 end
@@ -50,10 +49,10 @@ function test_cartesian_grid(tc)
 
 tname = "mini2dew_fang";
 
-test_dir = fullfile(tc.TestData.ref_dir, tname);
+test_dir = fullfile(tc.ref_dir, tname);
 %% get files if needed
 try
-  gemini3d.fileio.download_and_extract(tname, tc.TestData.ref_dir)
+  gemini3d.fileio.download_and_extract(tname, tc.ref_dir)
 catch e
   catcher(e, tc)
 end
@@ -62,7 +61,7 @@ cfg = gemini3d.read.config(test_dir);
 xg = gemini3d.grid.cartesian(cfg);
 
 % path patch
-cfg.outdir = tc.TestData.outdir;
+cfg.outdir = tc.outdir;
 cfg.indat_size = fullfile(cfg.outdir, cfg.indat_size);
 cfg.indat_grid = fullfile(cfg.outdir, cfg.indat_grid);
 
@@ -87,10 +86,10 @@ function test_Efield(tc)
 
 tname = "mini2dew_fang";
 
-test_dir = fullfile(tc.TestData.ref_dir, tname);
+test_dir = fullfile(tc.ref_dir, tname);
 %% get files if needed
 try
-  gemini3d.fileio.download_and_extract(tname, tc.TestData.ref_dir)
+  gemini3d.fileio.download_and_extract(tname, tc.ref_dir)
 catch e
   catcher(e, tc)
 end
@@ -98,14 +97,14 @@ end
 p = gemini3d.read.config(test_dir);
 tc.assumeNotEmpty(p, test_dir + " not contain config.nml")
 E0_dir = p.E0_dir;
-p.E0_dir = fullfile(tc.TestData.outdir, p.E0_dir);
+p.E0_dir = fullfile(tc.outdir, p.E0_dir);
 
 xg = gemini3d.read.grid(test_dir);
 
 gemini3d.efield.Efield_BCs(p, xg);
 
 time = p.times(1):seconds(p.dtE0):p.times(end);
-gemini3d.compare(fullfile(tc.TestData.outdir, E0_dir), fullfile(test_dir, E0_dir), only='efield', time=time)
+gemini3d.compare(fullfile(tc.outdir, E0_dir), fullfile(test_dir, E0_dir), only='efield', time=time)
 end
 
 
@@ -113,24 +112,24 @@ function test_precip(tc)
 
 tname = "mini2dew_fang";
 
-test_dir = fullfile(tc.TestData.ref_dir, tname);
+test_dir = fullfile(tc.ref_dir, tname);
 %% get files if needed
 try
-  gemini3d.fileio.download_and_extract(tname, tc.TestData.ref_dir)
+  gemini3d.fileio.download_and_extract(tname, tc.ref_dir)
 catch e
   catcher(e, tc)
 end
 %% setup new test data
 p = gemini3d.read.config(test_dir);
 prec_dir = p.prec_dir;
-p.prec_dir = fullfile(tc.TestData.outdir, p.prec_dir);
+p.prec_dir = fullfile(tc.outdir, p.prec_dir);
 
 xg = gemini3d.read.grid(test_dir);
 
 gemini3d.particles.particles_BCs(p, xg);
 
 try
-  gemini3d.compare(fullfile(tc.TestData.outdir, prec_dir), fullfile(test_dir, prec_dir), only="precip", time=p.times)
+  gemini3d.compare(fullfile(tc.outdir, prec_dir), fullfile(test_dir, prec_dir), only="precip", time=p.times)
 catch e
   if e.identifier == "gemini3d:compare:precip:allclose_error"
     tc.verifyFail("preciptation data didn't match reference")
@@ -143,13 +142,13 @@ end
 function test_gemini3d_run(tc, name)
 
 try
-  project_runner(tc, name, tc.TestData.ref_dir)
+  project_runner(tc, name, tc.ref_dir)
 catch err
   catcher(err, tc)
 end
 
 try
-  gemini3d.compare(tc.TestData.outdir, fullfile(tc.TestData.ref_dir, name), only="in")
+  gemini3d.compare(tc.outdir, fullfile(tc.ref_dir, name), only="in")
 catch e
   if e.identifier == "gemini3d:compare:input:allclose_error"
     tc.verifyFail("generated plasma data didn't match reference")
@@ -170,9 +169,9 @@ function test_plot_2d(tc)
 
 tname = "mini2dew_glow";
 
-project_runner(tc, tname, fullfile(tc.TestData.cwd, "data"))
+project_runner(tc, tname, fullfile(tc.cwd, "data"))
 
-data_dir = fullfile(tc.TestData.cwd, "data", tname);
+data_dir = fullfile(tc.cwd, "data", tname);
 
 % test 2D plots
 h = gemini3d.plot.frame(data_dir, datetime(2013, 2, 20, 5, 5, 0));
@@ -191,10 +190,10 @@ end
 function test_plot_3d(tc)
 tname = 'mini3d_glow';
 
-project_runner(tc, tname, fullfile(tc.TestData.cwd, "data"))
+project_runner(tc, tname, fullfile(tc.cwd, "data"))
 
 % test 3D plots
-d3 = fullfile(tc.TestData.cwd, "data", tname);
+d3 = fullfile(tc.cwd, "data", tname);
 h = gemini3d.plot.init(gemini3d.read.grid(d3));
 
 tc.verifySize(h, [1,10])
