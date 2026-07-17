c5c27bbdc94384d919ebcbf38af36a80203f6df6
scivision <scivision@users.noreply.github.com>
2026-07-17 12:20:49 -0400

    test: download: inform and skip if URL for ref data not working

--- test/private/catcher.m
+++ test/private/catcher.m
@@ -6,17 +6,23 @@ end
 
 if endsWith(err.message, ".parm not found")
   tc.assumeFail("MSIS 2 not enabled")
-elseif err.identifier == "gemini3d:model:msis:FileNotFoundError"
-  tc.assumeFail("msis_setup program not found. Compile with https://github.com/gemini3d/gemini3d/gemini3d.git")
 elseif contains(err.message, "HDF5 library version mismatched error")
   tc.assumeFail("HDF5 shared library conflict Matlab <=> system:  " + err.message)
 elseif contains(err.message, "GLIBCXX")
   tc.assumeFail("conflict in libstdc++ version between Matlab <=> system:  " + err.message)
-elseif err.identifier == "MATLAB:webservices:UnknownHost"
-  tc.assumeFail("no internet connection to website")
-elseif err.identifier == "MATLAB:webservices:SSLConnectionSystemFailure"
-  tc.assumeFail("HTTPS certificate error. Is environment variable SSL_CERT_FILE set? " + err.message)
-else
-  rethrow(err)
 end
+
+switch err.identifier
+  case 'gemini3d:model:msis:FileNotFoundError'
+   tc.assumeFail("msis_setup program not found. Compile with https://github.com/gemini3d/gemini3d/gemini3d.git")
+  case 'MATLAB:webservices:UnknownHost'
+    tc.assumeFail("no internet connection to website")
+  case 'MATLAB:webservices:SSLConnectionSystemFailure'
+    tc.assumeFail("HTTPS certificate error. Is environment variable SSL_CERT_FILE set? " + err.message)
+  case 'MATLAB:webservices:HTTP500StatusCodeError'
+    tc.assumeFail('Reference data server code 500 error. Is server down or is URL now invalid?')
+  otherwise
+    rethrow(err)
+end
+
 end
